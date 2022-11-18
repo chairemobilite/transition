fbc66ce714cf00f1294af655b6ab804f6e08a46a
Geneviève Bastien <gbastien@versatic.net>
2022-11-18 09:38:47 -0500

    config: Add default paths for project config file
    Fixes #425
    
    First, we check if the PROJECT_CONFIG environment variable contains the
    path to the config file. Otherwise, it is expected to be in the
    $homedir/.config/transition/config.js or /etc/transition/config.js.
    
    The path in the .env now has to be absolute or relative to the initial
    current working directory where the command was run. The ${rootDir} does
    not apply anymore.
    
    Add a start:demo script in the package.json which sets the
    PROJECT_CONFIG environment variable to the example config.

---
+++ packages/chaire-lib-backend/src/config/__tests__/server.config.noenv.test.ts
@@ -0,0 +1,29 @@
+// Test the server configuration without the environment variable, the file won't exists
+delete process.env.PROJECT_CONFIG;
+import fs from 'fs';
+import os from 'os';
+
+jest.mock('fs', () => {
+    // Require the original module to not be mocked...
+    const originalModule =
+        jest.requireActual<typeof import('fs')>('fs');
+  
+    return {
+        ...originalModule,
+        existsSync: jest.fn().mockReturnValue(false)
+    };
+});
+const existsSyncMock = fs.existsSync as jest.MockedFunction<typeof fs.existsSync>;
+
+test('Expected reading configuration files in default paths', () => {
+    let error: unknown | undefined = undefined;
+    try {
+        require('../server.config')
+    } catch (err) {
+        error = err;
+    }
+    expect(error).toBeDefined();
+    expect(existsSyncMock).toHaveBeenCalledTimes(2);
+    expect(existsSyncMock).toHaveBeenCalledWith(`${os.homedir}/.config/transition/config.js`);
+    expect(existsSyncMock).toHaveBeenCalledWith(`/etc/transition/config.js`);
+});
--- packages/chaire-lib-backend/src/config/server.config.ts
+++ packages/chaire-lib-backend/src/config/server.config.ts
@@ -5,21 +5,46 @@
  * License text available at https://opensource.org/licenses/MIT
  */
 import path from 'path';
+import os from 'os';
 
 import config, { setProjectConfiguration } from 'chaire-lib-common/lib/config/shared/project.config';
 import fs from 'fs';
 
-// TODO Add a default path for the project configuration file. Now, it fails
-if (typeof process.env.PROJECT_CONFIG !== 'string') {
-    throw 'You must set the PROJECT_CONFIG environment variable in the .env file with the path to the config file.';
-}
+const etcConfigFile = '/etc/transition/config.js';
+const homeDirConfigFileRelative = '.config/transition/config.js';
 
-const projectConfigFile = process.env.PROJECT_CONFIG.replace('${rootDir}', `${__dirname}/../../../../`);
+// INIT_CWD is the directory from which the script was run (usually root of the
+// repo), while PWD would be the chaire-lib-backend directory if run with `yarn
+// start`
+const workingDir = process.env.INIT_CWD || process.env.PWD;
 
-const configFileNormalized = path.normalize(projectConfigFile);
-if (!fs.existsSync(configFileNormalized)) {
-    throw `Configuration file '${configFileNormalized}' does not exist`;
-}
+// Get the config file. The return file path exists, the configuration file can
+// come from the PROJECT_CONFIG environment variable, then
+// $HOME/.config/transition/config.js, then /etc/transition/config.js
+const getConfigFilePath = (): string => {
+    // First, check the PROJECT_CONFIG environment variable
+    if (typeof process.env.PROJECT_CONFIG === 'string') {
+        const projectConfigFile = process.env.PROJECT_CONFIG.startsWith('/')
+            ? process.env.PROJECT_CONFIG
+            : `${workingDir}/${process.env.PROJECT_CONFIG}`;
+        if (fs.existsSync(projectConfigFile)) {
+            return path.normalize(projectConfigFile);
+        }
+        console.log(
+            `Configuration file specified by PROJECT_CONFIG environment variable does not exist: ${projectConfigFile}`
+        );
+    }
+    const homeDirConfigFile = path.normalize(`${os.homedir()}/${homeDirConfigFileRelative}`);
+    if (fs.existsSync(homeDirConfigFile)) {
+        return homeDirConfigFile;
+    }
+    if (fs.existsSync(etcConfigFile)) {
+        return etcConfigFile;
+    }
+    throw `Configuration file not found. Either set the PROJECT_CONFIG environment variable to point to the config file, or use files ${homeDirConfigFile} or ${etcConfigFile} paths`;
+};
+
+const configFileNormalized = getConfigFilePath();
 
 // Initialize default server side configuration
 setProjectConfiguration({
@@ -29,6 +54,7 @@ setProjectConfiguration({
 
 try {
     const configFromFile = require(configFileNormalized);
+    console.log(`Read server configuration from ${configFileNormalized}`);
     // Default project directory is `runtime` at the root of the repo
     if (configFromFile.projectDirectory === undefined && configFromFile.projectShortname !== undefined) {
         configFromFile.projectDirectory = path.normalize(
@@ -37,7 +63,7 @@ try {
     }
     setProjectConfiguration(configFromFile);
 } catch (error) {
-    console.error('Error loading server configuration from the PROJECT_CONFIG environment variable');
+    console.error(`Error loading server configuration in file ${configFileNormalized}`);
 }
 
 /** Application configuration, does not include the server configuration */
