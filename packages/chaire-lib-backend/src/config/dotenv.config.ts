4fa2433b01b0d928d577ac1333e26ce0a71c42a4
Geneviève Bastien <gbastien@versatic.net>
2022-11-18 12:12:40 -0500

    config: Add a TRANSITION_DOTENV environment variable
    Fixes #459
    
    The TRANSITION_DOTENV should point to the .env file to use, either
    absolute or relative to the initial working directory. By default,
    it is in the initial working directory where the command was first ran.

--- packages/chaire-lib-backend/src/config/dotenv.config.ts
+++ packages/chaire-lib-backend/src/config/dotenv.config.ts
@@ -5,8 +5,39 @@
  * License text available at https://opensource.org/licenses/MIT
  */
 import path from 'path';
+import fs from 'fs';
 
 if (!process.env.NODE_ENV) {
     process.env.NODE_ENV = 'development';
 }
-export default require('dotenv').config({ path: path.join(__dirname, '../../../..', '.env') });
+
+// INIT_CWD is the directory from which the script was run (usually root of the
+// repo), while PWD would be the chaire-lib-backend directory if run with `yarn
+// start`
+const workingDir = process.env.INIT_CWD || process.env.PWD;
+
+// Get the .env file. The return file path exists, the .env file can come from
+// the TRANSITION_DOTENV environment variable, or is expected to be at the root
+// of the directory
+const getDotEnvFile = (): string => {
+    // First, check the PROJECT_CONFIG environment variable
+    if (typeof process.env.TRANSITION_DOTENV === 'string') {
+        const dotEnvFile = process.env.TRANSITION_DOTENV.startsWith('/')
+            ? process.env.TRANSITION_DOTENV
+            : `${workingDir}/${process.env.TRANSITION_DOTENV}`;
+        if (fs.existsSync(dotEnvFile)) {
+            return path.normalize(dotEnvFile);
+        }
+        console.log(`.env file specified by TRANSITION_DOTENV environment variable does not exist: ${dotEnvFile}`);
+    }
+    const defaultDotEnv = `${workingDir}/.env`;
+    if (fs.existsSync(defaultDotEnv)) {
+        return defaultDotEnv;
+    }
+    // FIXME When all that is in .env right now can also be in the config file (issue #97), the .env will not be required anymore and the throw won't be necessary
+    throw `.env file not found. Set the TRANSITION_DOTENV environment variable to point to the .env file. The default path is ${defaultDotEnv}`;
+};
+
+const dotEnvPath = getDotEnvFile();
+console.log(`Using .env file from ${dotEnvPath}`);
+export default require('dotenv').config({ path: dotEnvPath });
