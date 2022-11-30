e684d950a0ab719a532222801b4a7797b0799f93
Geneviève Bastien <gbastien@versatic.net>
2022-11-29 19:29:41 -0500

    Use node's inspector feature instead of inspector api
    This removes a dependency, which was triggering `yarn` warnings anyway.
    They both produce the same kind of output.
    
    It was tested with node 16 and 12.9 (our minimal version) and the
    node inspector feature is available in both, at least on linux.
    
    Also append the date to the profiling data file and add it to the
    .gitignore.

--- packages/chaire-lib-backend/src/tasks/taskWrapper.ts
+++ packages/chaire-lib-backend/src/tasks/taskWrapper.ts
@@ -8,12 +8,28 @@ import '../config/dotenv.config';
 
 // Just make sure the config is initialized in the task, so tasks that are in common workspaces can have the right config
 import '../config/server.config';
-import Inspector from 'inspector-api';
+// The inspector feature does not have a version, it says "(none yet)" so eslint complained, disabling this rule
+// eslint-disable-next-line node/no-unsupported-features/node-builtins
+import Inspector from 'inspector';
+import moment from 'moment';
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 import { fileManager } from '../utils/filesystem/fileManager';
 
 import { GenericTask } from 'chaire-lib-common/lib/tasks/genericTask';
+
+// INIT_CWD is the directory from which the script was run (usually root of the
+// repo), while PWD would be the chaire-lib-backend directory if run with `yarn
+// start`
+const workingDir = process.env.INIT_CWD || process.env.PWD;
+
+const inpectorPost = async (inspector: Inspector.Session, message: string): Promise<unknown> => {
+    return new Promise((resolve, reject) => {
+        inspector.post(message, {}, (err: Error | null, params: unknown) =>
+            err === null ? resolve(params) : reject(err)
+        );
+    });
+};
 
 async function profilingWrapper(
     task: GenericTask,
@@ -21,24 +37,20 @@ async function profilingWrapper(
     argv: { [key: string]: unknown },
     profileFile: unknown
 ) {
-    // See if profiling is enabled for this task
-    const inspector = new Inspector();
+    const inspector = new Inspector.Session();
+    inspector.connect();
+
     try {
-        await inspector.profiler.enable();
-        await inspector.profiler.start();
+        await inpectorPost(inspector, 'Profiler.enable');
+        await inpectorPost(inspector, 'Profiler.start');
         await task.run(argv);
-        const profile = await inspector.profiler.stop();
+    } finally {
+        const profile = ((await inpectorPost(inspector, 'Profiler.stop')) as Inspector.Profiler.StopReturnType).profile;
         if (profileFile) {
             fileManager.writeFileAbsolute(profileFile, JSON.stringify(profile));
             console.log('Saved profiling data to %s', profileFile);
         }
-    } catch (error) {
-        const profile = await inspector.profiler.stop();
-        if (profileFile) {
-            fileManager.writeFileAbsolute(profileFile, JSON.stringify(profile));
-            console.log('Saved profiling data to %s', profileFile);
-        }
-        throw error;
+        await inpectorPost(inspector, 'Profiler.disable');
     }
 }
 
@@ -47,7 +59,11 @@ async function taskWrapper(task: GenericTask) {
     const argv = yargs(hideBin(process.argv)).argv;
 
     const doProfile = argv.profile;
-    const profileFile = doProfile ? (argv.to ? argv.to : __dirname + '/profilingData.json') : null;
+    const profileFile = doProfile
+        ? argv.to
+            ? argv.to
+            : `${workingDir}/profilingData_${moment().format('YYYYMMDD_HHmmss')}.cpuprofile`
+        : null;
     delete argv.profile;
     delete argv.to;
 
