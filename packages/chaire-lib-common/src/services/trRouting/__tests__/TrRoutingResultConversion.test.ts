410bb11085b60530b172747aef05d2f94e7dad4d
Geneviève Bastien <gbastien@versatic.net>
2022-12-23 14:34:22 -0500

    trRouting: Add a route result conversion function
    This function converts a route result in an object with human-readable
    field names, with units and various representations for each field (for
    examples, times in minutes and seconds). This result object can be used
    to export data from transition.

---
+++ packages/chaire-lib-common/src/services/trRouting/__tests__/TrRoutingResultConversion.test.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2022, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import _cloneDeep from 'lodash.clonedeep';
+import { pathNoTransferRouteResult, pathNoTransfer } from '../../../test/services/trRouting/TrRoutingConstantsStubs';
+import { routeToUserObject } from '../TrRoutingResultConversion';
+
+test('Test result conversion', () => {
+    // Change the few differences with the old path no transfer
+    const expected = _cloneDeep(pathNoTransfer) as any;
+    delete expected.optimizeCases;
+    delete expected.status;
+    expected.steps.forEach(step => {
+        step.action = step.action === 'board' ? 'boarding' : step.action === 'unboard' ? 'unboarding' : step.action;
+        if (step.action === 'walking' && step.readyToBoardAt === undefined) {
+            step.readyToBoardAt = undefined;
+            step.readyToBoardAtSeconds = undefined;
+        }
+    });
+    expect(routeToUserObject(pathNoTransferRouteResult)).toEqual(expected);
+});
+
+// TODO Cannot test the path with transfer because its data is all wrong. Make a real path first
