c3adb776ecf6f87934d54b3a92cea75d9e5cce4f
Geneviève Bastien <gbastien@versatic.net>
2023-03-09 15:38:19 -0500

    jobs: Add a JobCheckpointTracker class
    This class keeps track of possibly out of order chunks of work and
    notifies a progress emitter when a chunk of work has been completed.
    This is handy for long-running tasks that execute steps which can finish
    out of order, so the task can be resumed if stopped.

---
+++ packages/transition-backend/src/services/executableJob/JobCheckpointTracker.ts
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2023, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import { EventEmitter } from 'events';
+
+/**
+ * Class that allows to keep track of task execution progress. Ideal for jobs
+ * that include individual steps, this allows to inform the task when a chunk
+ * has been completed and save this checkpoint.
+ *
+ * Using this class is suggested for asynchronous steps, where steps can be
+ * completed out of order, to keep track of each individual step. Listeners can
+ * listen to the 'checkpoint' event.
+ */
+export class CheckpointTracker {
+    private indexes: number[] = [];
+    private lastCheckpointIdx: number;
+
+    /**
+     * Constructor
+     * @param chunkSize Number of steps in a chunk.
+     * @param progressEmitter event emitter that will be notified when chunks
+     * are finished. It will send a 'checkpoint' event with the checkpoint, ie
+     * chunk size * nb of terminated chunks
+     * @param currentCheckpoint If resuming a task, this is the last checkpoint
+     * that was registered in previous run
+     */
+    constructor(private chunkSize: number, private progressEmitter: EventEmitter, currentCheckpoint = 0) {
+        this.lastCheckpointIdx = Math.floor(currentCheckpoint / chunkSize) - 1;
+        // Add items to the last checkpoint, in case the chunk size is not a multiple of the current checkpoint
+        const lastChunkItems = currentCheckpoint % chunkSize;
+        if (lastChunkItems > 0) {
+            this.indexes[this.lastCheckpointIdx + 1] = lastChunkItems;
+        }
+    }
+
+    /**
+     * Tell the tracker that the step at index was completed
+     * @param handledIndex 0-based index of the step that was just completed
+     */
+    handled = (handledIndex: number): void => {
+        if (handledIndex < 0) {
+            console.log('Invalid index received in checkpoint tracker', handledIndex);
+            return;
+        }
+        // Increment the count for this checkpoint
+        const chkIndex = this.getCheckpointIndex(handledIndex);
+        this.indexes[chkIndex] = (this.indexes[chkIndex] || 0) + 1;
+        if (this.indexes[chkIndex] === this.chunkSize) {
+            // See if we need to notify for this checkopint
+            this.maybeNotifyCheckpoint(chkIndex);
+        }
+    };
+
+    private getCheckpointIndex = (handledIndex: number) => {
+        return Math.floor(handledIndex / this.chunkSize);
+    };
+
+    private maybeNotifyCheckpoint(chkIndex: number) {
+        // If the previous checkpoint is completed, then notify for the next
+        // checkpoint, otherwise do nothing, we need to wait for the previous to
+        // complete first
+        if (this.lastCheckpointIdx === chkIndex - 1) {
+            let indexToNotify = chkIndex;
+            // Get the last completed checkpoint
+            while (this.indexes[indexToNotify + 1] === this.chunkSize) {
+                indexToNotify++;
+            }
+            this.progressEmitter.emit('checkpoint', (indexToNotify + 1) * this.chunkSize);
+            this.lastCheckpointIdx = indexToNotify;
+        }
+    }
+
+    /**
+     * Call when all steps have been completed. If the last chunk is not full,
+     * it will emit a 'checkpoint' event with the last index handled.
+     */
+    completed = (): void => {
+        const lastChunkIndex = this.lastCheckpointIdx + 1;
+        const lastChunkSize = this.indexes[lastChunkIndex];
+        if (lastChunkSize !== undefined && lastChunkSize > 0) {
+            this.progressEmitter.emit('checkpoint', lastChunkIndex * this.chunkSize + lastChunkSize);
+        }
+    };
+}
---
+++ packages/transition-backend/src/services/executableJob/__tests__/JobCheckpointTracker.test.ts
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2023, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import { EventEmitter } from 'events';
+
+import { CheckpointTracker } from '../JobCheckpointTracker';
+
+const mockProgressEmitter = new EventEmitter();
+const mockCheckpoint = jest.fn();
+mockProgressEmitter.on('checkpoint', mockCheckpoint);
+
+beforeEach(() => {
+    mockCheckpoint.mockClear();
+})
+
+test('Test smaller number of handled data than chunk size', () => {
+    const tracker = new CheckpointTracker(10, mockProgressEmitter);
+    tracker.handled(0);
+    tracker.handled(1);
+    tracker.handled(2);
+    expect(mockCheckpoint).not.toHaveBeenCalled();
+});
+
+test('Test in order step completion', () => {
+    const chunkSize = 5;
+    const tracker = new CheckpointTracker(chunkSize, mockProgressEmitter);
+    for (let i = 0; i < 2 * chunkSize; i++) {
+        tracker.handled(i);
+    }
+    expect(mockCheckpoint).toHaveBeenCalledTimes(2);
+    expect(mockCheckpoint).toHaveBeenCalledWith(chunkSize);
+    expect(mockCheckpoint).toHaveBeenCalledWith(2 * chunkSize);
+});
+
+test('Test out of order step completion', () => {
+    const chunkSize = 5;
+    const tracker = new CheckpointTracker(chunkSize, mockProgressEmitter);
+    // handle the first 4
+    for (let i = 0; i < chunkSize - 1; i++) {
+        tracker.handled(i);
+    }
+    // handle 6th, make sure the checkpoint has not been called
+    tracker.handled(5);
+    expect(mockCheckpoint).not.toHaveBeenCalled();
+
+    // handle 5th, the checkpoint should have been called now
+    tracker.handled(4);
+    expect(mockCheckpoint).toHaveBeenCalledTimes(1);
+    expect(mockCheckpoint).toHaveBeenCalledWith(5);
+});
+
+test('Finish second chunk before first', () => {
+    const chunkSize = 5;
+    const tracker = new CheckpointTracker(chunkSize, mockProgressEmitter);
+    // handle the first 4
+    for (let i = 0; i < 4; i++) {
+        tracker.handled(i);
+    }
+    // handle 6th through 10th, make sure the checkpoint has not been called
+    for (let i = 5; i < 10; i++) {
+        tracker.handled(i);
+    }
+    expect(mockCheckpoint).not.toHaveBeenCalled();
+
+    // handle 5th, the checkpoint should have been called now, with number 10
+    tracker.handled(4);
+    expect(mockCheckpoint).toHaveBeenCalledTimes(1);
+    expect(mockCheckpoint).toHaveBeenCalledWith(2 * chunkSize);
+});
+
+test('Test the completed method', () => {
+    const chunkSize = 5;
+    const extraItems = chunkSize - 2;
+    const tracker = new CheckpointTracker(chunkSize, mockProgressEmitter);
+    // handle 2 chunks + a few items less
+    for (let i = 0; i < (2 * chunkSize) + extraItems; i++) {
+        tracker.handled(i);
+    }
+    // Expect the 2 checkpoints to have been called
+    expect(mockCheckpoint).toHaveBeenCalledTimes(2);
+    expect(mockCheckpoint).toHaveBeenCalledWith(chunkSize);
+    expect(mockCheckpoint).toHaveBeenCalledWith(2 * chunkSize);
+
+    // Call the completed method, expect a third checkpoint call
+    tracker.completed();
+    expect(mockCheckpoint).toHaveBeenCalledTimes(3);
+    expect(mockCheckpoint).toHaveBeenCalledWith(2 * chunkSize + extraItems);
+});
+
+test('Resume from checkpoint, multiple of chunk size', () => {
+    const chunkSize = 5;
+    const lastCheckpoint = 2 * chunkSize;
+    const tracker = new CheckpointTracker(chunkSize, mockProgressEmitter, lastCheckpoint);
+    // handle 2 other chunks
+    for (let i = lastCheckpoint; i <= 4 * chunkSize; i++) {
+        tracker.handled(i);
+    }
+    // Expect the 2 checkpoints to have been called
+    expect(mockCheckpoint).toHaveBeenCalledTimes(2);
+    expect(mockCheckpoint).toHaveBeenCalledWith(3 * chunkSize);
+    expect(mockCheckpoint).toHaveBeenCalledWith(4 * chunkSize);
+});
+
+test('Resume from checkpoint, with extra elements', () => {
+    const chunkSize = 5;
+    const lastCheckpoint = 2 * chunkSize + 2;
+    const tracker = new CheckpointTracker(chunkSize, mockProgressEmitter, lastCheckpoint);
+    // handle 2 other chunks
+    for (let i = lastCheckpoint; i <= 4 * chunkSize; i++) {
+        tracker.handled(i);
+    }
+    // Expect the 2 checkpoints to have been called
+    expect(mockCheckpoint).toHaveBeenCalledTimes(2);
+    expect(mockCheckpoint).toHaveBeenCalledWith(3 * chunkSize);
+    expect(mockCheckpoint).toHaveBeenCalledWith(4 * chunkSize);
+});
+
+test('Resume from checkpoint, smaller than chunk size', () => {
+    const chunkSize = 5;
+    const lastCheckpoint = chunkSize - 2;
+    const tracker = new CheckpointTracker(chunkSize, mockProgressEmitter, lastCheckpoint);
+    // handle 2 other chunks
+    for (let i = lastCheckpoint; i <= 2 * chunkSize; i++) {
+        tracker.handled(i);
+    }
+    // Expect the 2 checkpoints to have been called
+    expect(mockCheckpoint).toHaveBeenCalledTimes(2);
+    expect(mockCheckpoint).toHaveBeenCalledWith(chunkSize);
+    expect(mockCheckpoint).toHaveBeenCalledWith(2 * chunkSize);
+});
