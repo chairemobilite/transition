10dcf947dc51921c886e3fb664346641107089ba
Geneviève Bastien <gbastien@versatic.net>
2023-03-30 09:34:01 -0400

    tests: Mock the db.config file
    Fixes #633
    
    In unit tests testing files that import database query files, the
    database configuration is always loaded and requires the .env file to
    have been parsed with proper data. This should not be a requirement to
    run tests, especially that the database queries that are under test are
    already mocked. This adds a mock for the db.config, which simply
    initializes a dummy knex object for 'sqlite' that should make the tests
    pass without configuration.
    
    The `db.config` file is automatically mocked in chaire-lib-backend and
    transition-backend packages.

---
+++ packages/chaire-lib-backend/jestSetup.ts
@@ -0,0 +1,7 @@
+/*
+ * Copyright 2023, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+jest.mock('./src/config/shared/db.config');
