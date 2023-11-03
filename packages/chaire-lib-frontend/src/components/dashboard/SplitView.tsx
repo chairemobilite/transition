60a64a26f96809bb40521aa788d49325ab4429fb
David Murray <david.murray16@hotmail.com>
2023-11-03 10:45:39 -0400

    Small UI fixes (#751)
    * Use fixed-size elements to display the coordinates in the top toolbar.
    
    Otherwise, we get annoying flickers when some coordinates have fewer decimals. Also, set align-items to 'center' so that the coordinates are aligned with the other buttons.
    
    * Add missing copyright header
    
    * Change react-collapsible's arrow angles
    
    This now follows the more common convention of "right" being "closed" and "down" being "open".
    
    * Fix "lignes" header having two background colors
    
    * Use fixed-size elements to display the coordinates in the top toolbar.
    
    Otherwise, we get annoying flickers when some coordinates have fewer decimals. Also, set align-items to 'center' so that the coordinates are aligned with the other buttons.
    
    * Improve <select> styling
    
    Add rounded corners, a thinner border and set the same font size as <input> components
    
    * Add a custom white chevron for the select component
    
    Otherwise, it's black on black and we can't see it.

--- packages/chaire-lib-frontend/src/components/dashboard/SplitView.tsx
+++ packages/chaire-lib-frontend/src/components/dashboard/SplitView.tsx
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2023, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
 import React, { createRef, useEffect, useState } from 'react';
 
 // This components provides a resizable split panel.
