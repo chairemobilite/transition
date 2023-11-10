456250a50f77442385004366151527f74bae6fd1
davidmurray <david.murray16@hotmail.com>
2023-11-10 15:30:18 -0500

    WIP

--- deck-gl-pow/app.js
+++ deck-gl-pow/app.js
@@ -2,9 +2,16 @@ import React, {useState, useEffect} from 'react';
 import DeckGL from '@deck.gl/react';
 import {Map} from 'react-map-gl';
 import {TripsLayer} from '@deck.gl/geo-layers';
+import { PathStyleExtension } from '@deck.gl/extensions';
 import {createRoot} from 'react-dom/client';
 import { ScatterplotLayer } from 'deck.gl';
 import ScatterplotCustomLayer from './scatter-plot-custom-layer'
+import PathLineCustomLayer from './path-line-custom-layer'
+
+deck.log.enable();
+deck.log.level = 2;
+luma.log.enable();
+luma.log.level = 2;
 
 function getTooltip({object}) {
   return object && object.properties.name;
@@ -29,13 +36,16 @@ var nodeIndex = -1;
 
 var routeSelected = null; 
 var nodeSelected = null;
-
+console.log("Hello");
 export default function Counter({routeData, nodeData}) {
   const [time, setTime] = useState(0);
+  const [dashStart, setDashStart] = useState(0);
+
   const [animation] = useState({});
 
   const animate = () => {
     setTime(t => (t + 1) % 700);
+    setDashStart((Date.now() / 100) % 1000);
     animation.id = window.requestAnimationFrame(animate);
   };
 
@@ -75,29 +85,33 @@ export default function Counter({routeData, nodeData}) {
       }
     }),
 
-    new TripsLayer({
+    new PathLineCustomLayer({
       id: 'trips-layer-selected',
       data: routeSelected,
       getPath: d => d.geometry.coordinates,
-      getTimestamps: d => {setTimestamps(d);},
-      opacity: 0.8,
-      widthMinPixels: 2,
-      rounded: true,
-      fadeTrail: true,
-      trailLength: 400,
-      currentTime: parseFloat(Math.cos(time/20)*100 + 200),
-      shadowEnabled: false,
+      //getTimestamps: d => {setTimestamps(d);},
+      //opacity: 0.8,
+      //widthMinPixels: 2,
+      //rounded: true,
+      //fadeTrail: true,
+      //trailLength: 400,
+      //currentTime: parseFloat(Math.cos(time/20)*100 + 200),
+      //shadowEnabled: false,
       pickable: true,
-      getColor: d => {
+      /*getColor: d => {
         const rgb = d.properties.color
         return [200, 200, 0]
-      },
+      },*/
       getWidth: (d, i) => {
         if(i.index === routeIndex) {
           return 70;
         }
         return 20;
       },
+      getDashArray: [4, 4],
+      getOffset: 20,
+      dashStart: dashStart,
+      extensions: [new PathStyleExtension({highPrecisionDash: true })]
     }),
     
     new ScatterplotLayer({
@@ -111,7 +125,7 @@ export default function Counter({routeData, nodeData}) {
         return [parseInt(rgb.substring(1,3), 16), parseInt(rgb.substring(3,5), 16), parseInt(rgb.substring(5), 16),255]
       },
       getLineColor: [255,255,255,255],
-      getRadius: 100,
+      getRadius: 400,
       extensions: [new ScatterplotCustomLayer()]
     }),
 
