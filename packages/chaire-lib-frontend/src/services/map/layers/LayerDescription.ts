4f9c901b6916aed216b1a6b696c0286586f07c83
Geneviève Bastien <gbastien@versatic.net>
2023-11-15 14:52:20 -0500

    deck.gl: Change types of the map layer
    The type is named `MapLayer` and another type `LayerConfiguration` is used
    to configure the layer by the application. The configuration is meant to
    be fixed by the application and does not change, except through
    Preferences, if necessary.

--- packages/chaire-lib-frontend/src/services/map/layers/LayerDescription.ts
+++ packages/chaire-lib-frontend/src/services/map/layers/LayerDescription.ts
@@ -5,17 +5,25 @@
  * License text available at https://opensource.org/licenses/MIT
  */
 
-export type LayerDescription = {
+export type LayerConfiguration = {
+    // TODO Type this properly. When the data in layers.config.ts is used by the new API, add it here
+    [key: string]: any;
+};
+
+export type MapLayer = {
     /** Unique identifier for this layer */
     id: string;
-    /** Whether the layer is visible, if enabled */
+    /**
+     * Whether the layer is visible. The layer visibility is typically defined
+     * by the user. It does not mean it is always visible. It is only visible
+     * when it is enabled by the application.
+     */
     visible: boolean;
     /**
-     * Description of the current layer
-     *
-     * TODO Type this properly
+     * Configuration of the current layer. It should be fixed and not require
+     * updates, except through Preferences if any
      */
-    layerDescription: { [key: string]: any };
+    configuration: LayerConfiguration;
     /** Features contained in this layer */
     layerData: GeoJSON.FeatureCollection<GeoJSON.Geometry>;
 };
