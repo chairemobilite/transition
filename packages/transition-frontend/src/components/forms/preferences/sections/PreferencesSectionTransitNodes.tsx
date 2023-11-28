e25dd7fd2730c3d82136bd26abd4646598be9f6e
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2023-11-28 17:06:25 -0500

    add default dwell time seconds to prefs ui for nodes

--- packages/transition-frontend/src/components/forms/preferences/sections/PreferencesSectionTransitNodes.tsx
+++ packages/transition-frontend/src/components/forms/preferences/sections/PreferencesSectionTransitNodes.tsx
@@ -5,12 +5,15 @@
  * License text available at https://opensource.org/licenses/MIT
  */
 import React from 'react';
+import _toString from 'lodash/toString';
 import Collapsible from 'react-collapsible';
 import { withTranslation } from 'react-i18next';
 import PreferencesResetToDefaultButton from '../PreferencesResetToDefaultButton';
 import InputWrapper from 'chaire-lib-frontend/lib/components/input/InputWrapper';
+import InputStringFormatted from 'chaire-lib-frontend/lib/components/input/InputStringFormatted';
 import InputColor from 'chaire-lib-frontend/lib/components/input/InputColor';
 import PreferencesSectionProps from '../PreferencesSectionProps';
+import { parseFloatOrNull } from 'chaire-lib-common/lib/utils/MathUtils';
 
 const PreferencesSectionTransitNodes: React.FunctionComponent<PreferencesSectionProps> = (
     props: PreferencesSectionProps
@@ -20,7 +23,7 @@ const PreferencesSectionTransitNodes: React.FunctionComponent<PreferencesSection
     return (
         <Collapsible trigger={props.t('transit:transitNode:Nodes')} open={true} transitionTime={100}>
             <div className="tr__form-section">
-                <InputWrapper twoColumns={true} label={props.t('main:preferences:DefaultColor')}>
+                <InputWrapper twoColumns={true} key="defaultColor" label={props.t('main:preferences:DefaultColor')}>
                     <InputColor
                         defaultColor={prefs.transit.nodes.defaultColor}
                         id={'formFieldPreferencesTransitNodeDefaultColor'}
@@ -32,6 +35,26 @@ const PreferencesSectionTransitNodes: React.FunctionComponent<PreferencesSection
                     <PreferencesResetToDefaultButton
                         resetPrefToDefault={props.resetPrefToDefault}
                         path="transit.nodes.defaultColor"
+                        preferences={props.preferences}
+                    />
+                </InputWrapper>
+                <InputWrapper
+                    twoColumns={true}
+                    key="defaultDwellTimeSeconds"
+                    label={props.t('transit:transitNode:DefaultDwellTimeSeconds')}
+                >
+                    <InputStringFormatted
+                        id={'formFieldPreferencesTransitNodeDefaultDwellTimeSeconds'}
+                        value={prefs.transit.nodes.defaultDwellTimeSeconds}
+                        onValueUpdated={(value) => props.onValueChange('transit.nodes.defaultDwellTimeSeconds', value)}
+                        key={`formFieldPreferencesTransitNodeDefaultDwellTimeSeconds_${props.resetChangesCount}`}
+                        stringToValue={parseFloatOrNull}
+                        valueToString={(val) => _toString(parseFloatOrNull(val))}
+                        type="number"
+                    />
+                    <PreferencesResetToDefaultButton
+                        resetPrefToDefault={props.resetPrefToDefault}
+                        path={'transit.nodes.defaultDwellTimeSeconds'}
                         preferences={props.preferences}
                     />
                 </InputWrapper>
