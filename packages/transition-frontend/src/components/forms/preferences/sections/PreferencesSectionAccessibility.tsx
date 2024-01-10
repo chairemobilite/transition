56ebbaead770c392694eb45826fbb493bad1d5d2
Geneviève Bastien <gbastien@versatic.net>
2024-01-10 09:40:59 -0500

    preferences: Add a section to set the accessibility map colors
    This allows to specify the accessibility map's location and polygon
    color in the user preferences.

---
+++ packages/transition-frontend/src/components/forms/preferences/sections/PreferencesSectionAccessibility.tsx
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2024, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import React from 'react';
+import Collapsible from 'react-collapsible';
+import { withTranslation } from 'react-i18next';
+import PreferencesSectionProps from '../PreferencesSectionProps';
+import PreferencesColorComponent from '../PreferencesColorComponent';
+
+const PreferencesSectionTransitRouting: React.FunctionComponent<PreferencesSectionProps> = (
+    props: PreferencesSectionProps
+) => (
+    <Collapsible trigger={props.t('transit:transitRouting:AccessibilityMap')} open={true} transitionTime={100}>
+        <div className="tr__form-section">
+            <PreferencesColorComponent
+                {...props}
+                prefPath="transit.routing.transitAccessibilityMap.locationColor"
+                label={props.t('transit:preferences:transitAccessibilityMap:LocationColor')}
+            />
+            <PreferencesColorComponent
+                {...props}
+                prefPath="transit.routing.transitAccessibilityMap.polygonColor"
+                label={props.t('transit:preferences:transitAccessibilityMap:PolygonColor')}
+            />
+        </div>
+    </Collapsible>
+);
+
+export default withTranslation(['main', 'transit'])(PreferencesSectionTransitRouting);
