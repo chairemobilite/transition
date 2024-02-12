d59f71e872b4dd0e1b269c17aac3d1dcd67d6abf
Maximus Shymko <maximus.shymko@polymtl.ca>
2024-02-11 22:27:23 -0500

    public.routes.ts: Added endpoints for paths, nodes, scenarios and routing modes

--- packages/transition-backend/src/api/public.routes.ts
+++ packages/transition-backend/src/api/public.routes.ts
@@ -6,6 +6,9 @@
  */
 import express from 'express';
 import { PassportStatic } from 'passport';
+import transitObjectEndpointDefinitions from './definitions/transitObjects.definitions';
+import { RoutingOrTransitMode } from 'chaire-lib-common/lib/config/routingModes';
+import osrmProcessManager from 'chaire-lib-backend/lib/utils/processManagers/OSRMProcessManager';
 
 export default function (app: express.Express, passport: PassportStatic) {
     const router = express.Router();
@@ -16,5 +19,26 @@ export default function (app: express.Express, passport: PassportStatic) {
 
     router.post('/', (req, res) => {
         res.send('The public API endpoint works!');
+    });
+
+    router.post('/paths', async (req, res) => {
+        const geojson = await transitObjectEndpointDefinitions.paths.geojsonCollection!();
+        res.json(geojson);
+    });
+
+    router.post('/nodes', async (req, res) => {
+        const geojson = await transitObjectEndpointDefinitions.nodes.geojsonCollection!();
+        res.json(geojson);
+    });
+
+    router.post('/scenarios', async (req, res) => {
+        const attributes = await transitObjectEndpointDefinitions.scenarios.collection!(null);
+        res.json(attributes);
+    });
+
+    router.post('/routing-modes', async (req, res) => {
+        const routingModes: RoutingOrTransitMode[] = await osrmProcessManager.availableRoutingModes();
+        routingModes.push('transit');
+        res.json(routingModes)
     });
 }
