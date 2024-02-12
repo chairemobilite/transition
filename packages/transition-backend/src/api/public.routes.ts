85280234902c314a45a8a1c790a9e91761ad1d3c
Maximus Shymko <maximus.shymko@polymtl.ca>
2024-02-11 22:02:55 -0500

    serverApp: added licence to public.routes.ts and removed superfluous import

--- packages/transition-backend/src/api/public.routes.ts
+++ packages/transition-backend/src/api/public.routes.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2024, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
 import express from 'express';
 import { PassportStatic } from 'passport';
 
