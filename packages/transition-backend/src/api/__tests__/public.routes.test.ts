ade9ff0b08689a3fc6a4e32f38285af23db5fef0
Maximus Shymko <maximus.shymko@polymtl.ca>
2024-02-13 19:02:43 -0500

    public.routes.test.ts: Refactored to make it easier to add more tests later

--- packages/transition-backend/src/api/__tests__/public.routes.test.ts
+++ packages/transition-backend/src/api/__tests__/public.routes.test.ts
@@ -1,33 +1,39 @@
-import supertest from 'supertest';
+/*
+ * Copyright 2024, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import request from 'supertest';
 import express from 'express';
 import publicRoutes from '../public.routes';
-import passport from 'passport'
+import passport from 'passport';
 
+// Mock passport (therefore ignoring authentication)
+jest.mock('passport');
+(passport.authenticate as jest.Mock).mockImplementation(() => {
+    return (req, res, next) => {
+        next();
+    }
+});
 
+beforeEach(() => {
+    jest.clearAllMocks();
+});
 
-jest.mock('passport')
+test('Authentication setup', () => {
+    publicRoutes(express(), passport);
+    expect(passport.authenticate).toBeCalledWith('api-strategy', {"failWithError": true, "failureMessage": true});
+});
 
-const mockApp = express();
+describe('Testing endpoints', () => {
+    const app = express();
+    publicRoutes(app, passport);
 
-describe("Name", () => {
-beforeEach(() => jest.clearAllMocks());
-    test('Bypass Authentication - Return value', async () => {
+    test('POST /api', async () => {
+        const response = await request(app).post('/api');
 
-
-        const passStub = jest.fn().mockImplementation((strategy, options) => {
-            return (req, res, next) => {
-                next();
-            };
-        });
-        
-        passport.authenticate = passStub
-        
-        publicRoutes(mockApp, passport)
-
-    const response = await supertest(mockApp).get("/api/")
-
-    expect(response.status).toEqual(200);
-    expect(response.text).toEqual('The public API endpoint works!')
-    expect(passStub).toBeCalledWith('api-strategy', {"failWithError": true, "failureMessage": true})
+        expect(response.status).toStrictEqual(200);
+        expect(response.text).toStrictEqual('The public API endpoint works!');
     });
 });
