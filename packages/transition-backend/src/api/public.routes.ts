d24ab37d0e9a8df3e2a163b020407ff7501015be
Ania Ben Abdesselam <ania.ben-abdesselam@polymtl.ca>
2024-02-19 23:14:58 -0600

    add get od

--- packages/transition-backend/src/api/public.routes.ts
+++ packages/transition-backend/src/api/public.routes.ts
@@ -4,12 +4,18 @@
  * This file is licensed under the MIT License.
  * License text available at https://opensource.org/licenses/MIT
  */
+
 import express from 'express';
 import { PassportStatic } from 'passport';
 import tokensDbQueries from 'chaire-lib-backend/lib/models/db/tokens.db.queries';
 import transitObjectDataHandlers from '../services/transitObjects/TransitObjectsDataHandler';
 import { RoutingOrTransitMode } from 'chaire-lib-common/lib/config/routingModes';
 import osrmProcessManager from 'chaire-lib-backend/lib/utils/processManagers/OSRMProcessManager';
+import osrmService from 'chaire-lib-backend/lib/utils/osrm/OSRMService';
+import trRoutingProcessManager from 'chaire-lib-backend/lib/utils/processManagers/TrRoutingProcessManager';
+import trRoutingService from 'chaire-lib-backend/lib/utils/trRouting/TrRoutingServiceBackend';
+
+
 
 export default function (app: express.Express, passport: PassportStatic) {
     app.use('/token', passport.authenticate('local-login', { failWithError: true, failureMessage: true }));
@@ -45,4 +51,27 @@ export default function (app: express.Express, passport: PassportStatic) {
     });
 
     app.use('/api', router);
+
+    router.get('/od', async (req, res) => {
+        const routingParameters = req.body.routingParameters;
+        if ((await trRoutingProcessManager.status({})).status === "not_running") {
+            await trRoutingProcessManager.restart({});
+        }
+
+        const routingResults = await trRoutingService.route(routingParameters);
+        const parameters = {
+            mode: req.body.mode,
+            points: routingParameters.originDestination
+        };
+        const routingResultsGeoJson = await osrmService.route(parameters);
+
+        const withGeo = req.body.withGeo !== undefined ? req.body.withGeo : true;
+        if (req.body.withGeo == false ) {
+            res.json(routingResults);
+
+        }
+        else {
+            res.json({ routingResults: routingResults, routingResultsGeoJson: routingResultsGeoJson });
+        }
+    });
 }
