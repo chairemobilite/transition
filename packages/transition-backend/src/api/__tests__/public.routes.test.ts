17d8b15c6775381b5bb07703205ee99f941b8547
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2024-03-05 18:20:35 -0500

    test - wip

--- packages/transition-backend/src/api/__tests__/public.routes.test.ts
+++ packages/transition-backend/src/api/__tests__/public.routes.test.ts
@@ -11,6 +11,11 @@ import request from 'supertest';
 import transitObjectDataHandlers from '../../services/transitObjects/TransitObjectsDataHandler';
 import osrmProcessManager from 'chaire-lib-backend/lib/utils/processManagers/OSRMProcessManager';
 import tokensDbQueries from 'chaire-lib-backend/lib/models/db/tokens.db.queries';
+import trRoutingService from 'chaire-lib-backend/lib/utils/trRouting/TrRoutingServiceBackend';
+import * as TrRoutingApi from 'chaire-lib-common/lib/api/TrRouting';
+import osrm from 'osrm';
+import * as Status from 'chaire-lib-common/lib/utils/Status';
+import osrmService from 'chaire-lib-backend/lib/utils/osrm/OSRMService';
 
 // Mock passport (therefore ignoring authentication)
 jest.mock('passport');
@@ -86,5 +91,73 @@ describe('Testing endpoints', () => {
         expect(response.status).toStrictEqual(200);
         expect(response.body).toStrictEqual(['transit']);
         expect(osrmProcessManager.availableRoutingModes).toBeCalled();
+    });
+
+    describe('Testing endpoints', () => {
+        const trRoutingServiceResult: TrRoutingApi.TrRoutingV2.RouteResponse = {
+            status: 'success',
+            query: {
+                origin: [45, 45],
+                destination: [45, 45],
+                timeOfTrip: 1800,
+                timeType: 0
+            },
+            result: {
+                routes: [],
+                totalRoutesCalculated: 1
+            }
+        }
+        const routeResults: Status.Status<osrm.RouteResults> = {
+            status: 'ok',
+            result: {
+                waypoints: [{
+                    distance: 45,
+                    name: "test",
+                    location: [45, 45],
+                    hint: "test"
+                }],
+                routes: []
+            }
+        }
+        
+        test('GET /api/route/:withGeometry? without optimal parameter', async () => {
+            trRoutingService.route = jest.fn()
+            osrmService.route = jest.fn()
+    
+            const response = await request(app).get('/api/route');
+    
+            expect(response.status).toStrictEqual(200);
+            // expect(response.body).toStrictEqual(['transit']);
+            expect(trRoutingService.route).toBeCalled();
+            expect(osrmService.route).toBeCalled();
+    
+        });
+    
+        test('GET /api/route/:withGeometry? optimal parameter true', async () => {
+            trRoutingService.route = jest.fn()
+            osrmService.route = jest.fn()
+    
+            const response = await request(app).get('/api/route/true');
+    
+            expect(response.status).toStrictEqual(200);
+            // expect(response.body).toStrictEqual(['transit']);
+            expect(trRoutingService.route).toBeCalled();
+            expect(osrmService.route).toBeCalled();
+    
+        }); 
+
+        test('GET /api/route/:withGeometry? optimal parameter false', async () => {
+            trRoutingService.route = jest.fn(() => Promise.resolve(trRoutingServiceResult))
+            osrmService.route = jest.fn(() => Promise.resolve(routeResults))
+    
+            const response = await request(app).get('/api/route/false');
+    
+            expect(response.status).toStrictEqual(200);
+            // expect(response.body).toStrictEqual(['transit']);
+            expect(trRoutingService.route).toBeCalled();
+            expect(osrmService.route).not.toBeCalled();
+    
+        }); 
+
     });
 });
