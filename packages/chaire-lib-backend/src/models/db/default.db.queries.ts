7d27e40a00c81a170b1d1b50a8cebb4bb1faf83b
Vincent Haney <vincent.haney@polytml.ca>
2024-04-16 16:19:41 -0400

    Token-Expiry:  Clean db file

--- packages/chaire-lib-backend/src/models/db/default.db.queries.ts
+++ packages/chaire-lib-backend/src/models/db/default.db.queries.ts
@@ -207,7 +207,6 @@ const updateMultiple = async <T extends GenericAttributes, U>(
 
 const deleteRecord = async (knex: Knex, tableName: string, id: string) => {
     try {
-
         if (!uuidValidate(id)) {
             throw new TrError(
                 `Cannot verify if object exists in table ${tableName} because the required parameter id is missing, blank or not a valid uuid`,
