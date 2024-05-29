65d4914e12efc33a661e3809927ddbe799f5bb8f
Yannick Brosseau <yannick.brosseau@gmail.com>
2024-05-29 14:13:21 -0400

    pyTransition: bump version

--- pyTransition/setup.py
+++ pyTransition/setup.py
@@ -25,7 +25,7 @@ from pathlib import Path
 
 setup(
     name="pyTransition",
-    version="0.1.1",
+    version="0.1.2",
     description="A Python package to interact with the Transition API.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
