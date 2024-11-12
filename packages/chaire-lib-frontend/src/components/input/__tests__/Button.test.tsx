9028a5271bad5ef735c83062c52d91d46512ab55
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2024-11-12 12:31:55 -0500

    upgrade @fortawesome packages
    needs to update snapshots too as some icons have been updated upstream
    
    also format affected test files

--- packages/chaire-lib-frontend/src/components/input/__tests__/Button.test.tsx
+++ packages/chaire-lib-frontend/src/components/input/__tests__/Button.test.tsx
@@ -7,16 +7,16 @@
 import * as React from 'react';
 import { create } from 'react-test-renderer';
 import { Button } from '../Button';
-import { render, fireEvent } from "@testing-library/react";
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { render, fireEvent } from '@testing-library/react';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 const mockOnClick = jest.fn();
-const testLabel = "Test";
+const testLabel = 'Test';
 
 test('Default props', () => {
     const button = create(<Button
-        onClick = {mockOnClick}
-        label = {testLabel}
+        onClick={mockOnClick}
+        label={testLabel}
     />)
         .toJSON();
     expect(button).toMatchSnapshot();
@@ -24,35 +24,35 @@ test('Default props', () => {
 
 test('Test overriding props with defaults', () => {
     const button = create(<Button
-        onClick = {mockOnClick}
-        label = {testLabel}
-        align = "left"
-        type = "submit"
-        color = "red"
-        size = "small"
-    />)
+        onClick={mockOnClick}
+        label={testLabel}
+        align="left"
+        type="submit"
+        color="red"
+        size="small"
+    />);
     expect(button).toMatchSnapshot();
 });
 
 test('Test disabled', () => {
     const button = create(<Button
-        onClick = {mockOnClick}
-        label = {testLabel}
-        align = "left"
-        type = "submit"
-        color = "red"
-        size = "small"
-        disabled = {true}
-    />)
+        onClick={mockOnClick}
+        label={testLabel}
+        align="left"
+        type="submit"
+        color="red"
+        size="small"
+        disabled={true}
+    />);
     expect(button).toMatchSnapshot();
 });
 
 test('Test on click', () => {
     mockOnClick.mockClear();
     const { getByText } = render(<Button
-        onClick = {mockOnClick}
-        label = {testLabel}
-    />)
+        onClick={mockOnClick}
+        label={testLabel}
+    />);
     const element = getByText(testLabel);
     fireEvent.click(element);
     expect(mockOnClick).toHaveBeenCalledTimes(1);
@@ -60,31 +60,31 @@ test('Test on click', () => {
 
 test('Test invisible', () => {
     const { queryByText } = render(<Button
-        onClick = {mockOnClick}
-        label = {testLabel}
-        isVisible = {false}
-    />)
+        onClick={mockOnClick}
+        label={testLabel}
+        isVisible={false}
+    />);
     const element = queryByText(testLabel);
     expect(element).toBeFalsy();
 });
 
 test('Test icon path', () => {
     const button = create(<Button
-        onClick = {mockOnClick}
-        label = {testLabel}
-        iconPath = "path/to/my/icon.png"
-        iconClass = "myIconClass"
-    />)
+        onClick={mockOnClick}
+        label={testLabel}
+        iconPath="path/to/my/icon.png"
+        iconClass="myIconClass"
+    />);
     expect(button).toMatchSnapshot();
 });
 
 test('Test icon', () => {
     const button = create(<Button
-        onClick = {mockOnClick}
-        label = {testLabel}
-        icon = {faCoffee}
-        iconClass = "myIconClass"
-    />)
+        onClick={mockOnClick}
+        label={testLabel}
+        icon={faCoffee}
+        iconClass="myIconClass"
+    />);
     expect(button).toMatchSnapshot();
 });
 
