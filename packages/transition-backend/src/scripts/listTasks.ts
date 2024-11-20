2c770b3a1001bad232a9e3012d7a7cadfe278ad7
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2024-11-20 13:35:27 -0500

    Update glob package and support only node 22
    Needed to update the import as well for the new version
    glob new version supports only node 20.

--- packages/transition-backend/src/scripts/listTasks.ts
+++ packages/transition-backend/src/scripts/listTasks.ts
@@ -4,7 +4,7 @@
  * This file is licensed under the MIT License.
  * License text available at https://opensource.org/licenses/MIT
  */
-import glob from 'glob';
+import * as glob from 'glob';
 import { listTasks as listLibTasks } from 'chaire-lib-backend/lib/scripts/listTasks';
 
 export const listTasks = (): string[] => {
