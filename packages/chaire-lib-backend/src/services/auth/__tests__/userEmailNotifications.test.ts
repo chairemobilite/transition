2c862e593f6cfd95f9c8292a09ba92bda9bc6101
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2024-11-21 14:08:39 -0500

    update nodemailer to v6.9.16 and mock to v2.0.6

--- packages/chaire-lib-backend/src/services/auth/__tests__/userEmailNotifications.test.ts
+++ packages/chaire-lib-backend/src/services/auth/__tests__/userEmailNotifications.test.ts
@@ -45,14 +45,14 @@ const frenchAdmin = {
     first_name: 'French',
     last_name: 'Admin',
     preferences: { lang: 'fr' }
-}
+};
 const englishAdmin = {
     id: 2,
     uuid: uuidV4(),
     email: 'english@transition.city',
     username: 'englishAdmin',
     preferences: { lang: 'en' }
-}
+};
 
 const confirmUrl = 'http://transition.city/verify/something';
 
@@ -60,10 +60,10 @@ beforeEach(() => {
     nodemailerMock.mock.reset();
     mockGetList.mockClear();
     mockGetList.mockResolvedValue({ users: [frenchAdmin, englishAdmin], totalCount: 2 });
-})
+});
 
 test('Test sending with defaults', async () => {
-    const user = new UserModel({...defaultUserData})
+    const user = new UserModel({ ...defaultUserData });
     await sendConfirmationEmail(user, { confirmUrl });
     const sentEmails = nodemailerMock.mock.getSentMail();
     expect(sentEmails.length).toBe(1);
@@ -71,13 +71,13 @@ test('Test sending with defaults', async () => {
     expect(sentEmails[0].to).toEqual(defaultUserData.email);
     expect(sentEmails[0].subject).toBeDefined();
     expect(sentEmails[0].text).toBeDefined();
-    
+
 });
 
 test('Test sending with various languages', async () => {
-    const frenchUser = new UserModel({...defaultUserData, preferences: { lang: 'fr' }})
+    const frenchUser = new UserModel({ ...defaultUserData, preferences: { lang: 'fr' } });
     await sendConfirmationEmail(frenchUser, { confirmUrl });
-    const englishUser = new UserModel({...defaultUserData, preferences: { lang: 'en' }})
+    const englishUser = new UserModel({ ...defaultUserData, preferences: { lang: 'en' } });
     await sendConfirmationEmail(englishUser, { confirmUrl });
 
     const sentEmails = nodemailerMock.mock.getSentMail();
@@ -91,7 +91,7 @@ test('Test sending with various languages', async () => {
 });
 
 test('Registration email confirm by admin', async () => {
-    const user = new UserModel({...defaultUserData})
+    const user = new UserModel({ ...defaultUserData });
     await sendConfirmationEmail(user, { strategy: 'confirmByAdmin', confirmUrl });
 
     const sentEmails = nodemailerMock.mock.getSentMail();
@@ -112,15 +112,15 @@ test('Registration email confirm by admin', async () => {
     expect(sentEmails[0].html).toContain('Bonjour ' + frenchAdmin.first_name + ' ' + frenchAdmin.last_name);
     expect(sentEmails[1].html).toContain('Hi ' + englishAdmin.username);
     expect(sentEmails[1].html).toContain(`<a href="${confirmUrl}">${confirmUrl}</a>`);
-    expect(sentEmails[0].html.match(/\<br\/\>/g).length).toBeGreaterThan(1);
+    expect((sentEmails[0].html as any).match(/\<br\/\>/g).length).toBeGreaterThan(1);
     expect(sentEmails[2].text).toContain('Bonjour');
     expect(sentEmails[2].text).not.toContain(confirmUrl);
 
 });
 
 test('Registration email confirm by admin, but no admin', async () => {
-    mockGetList.mockResolvedValueOnce({ users: [], totalCount: 0});
-    const user = new UserModel({...defaultUserData})
+    mockGetList.mockResolvedValueOnce({ users: [], totalCount: 0 });
+    const user = new UserModel({ ...defaultUserData });
     await expect(sendConfirmationEmail(user, { strategy: 'confirmByAdmin', confirmUrl }))
         .rejects
         .toEqual(new Error('There are no admins to confirm emails!!'));
@@ -129,7 +129,7 @@ test('Registration email confirm by admin, but no admin', async () => {
 });
 
 test('Forgot password email, french', async () => {
-    const user = new UserModel({...defaultUserData})
+    const user = new UserModel({ ...defaultUserData });
     await resetPasswordEmail(user, { resetPasswordUrl: confirmUrl });
 
     const sentEmails = nodemailerMock.mock.getSentMail();
@@ -142,11 +142,11 @@ test('Forgot password email, french', async () => {
     expect(sentEmails[0].text).toContain('\n');
     expect(sentEmails[0].html).toContain('Bonjour ');
     expect(sentEmails[0].html).toContain(`<a href="${confirmUrl}">${confirmUrl}</a>`);
-    expect(sentEmails[0].html.match(/\<br\/\>/g).length).toBeGreaterThan(1);
+    expect((sentEmails[0].html as any).match(/\<br\/\>/g).length).toBeGreaterThan(1);
 });
 
 test('Forgot password email, english', async () => {
-    const user = new UserModel({...defaultUserData, preferences: { lang: 'en' }})
+    const user = new UserModel({ ...defaultUserData, preferences: { lang: 'en' } });
     await resetPasswordEmail(user, { resetPasswordUrl: confirmUrl });
 
     const sentEmails = nodemailerMock.mock.getSentMail();
@@ -159,11 +159,11 @@ test('Forgot password email, english', async () => {
     expect(sentEmails[0].text).toContain('\n');
     expect(sentEmails[0].html).toContain('Hi ');
     expect(sentEmails[0].html).toContain(`<a href="${confirmUrl}">${confirmUrl}</a>`);
-    expect(sentEmails[0].html.match(/\<br\/\>/g).length).toBeGreaterThan(1);
+    expect((sentEmails[0].html as any).match(/\<br\/\>/g).length).toBeGreaterThan(1);
 });
 
 test('Confirmed by admin email', async () => {
-    const user = new UserModel({...defaultUserData})
+    const user = new UserModel({ ...defaultUserData });
     await sendConfirmedByAdminEmail(user);
 
     const sentEmails = nodemailerMock.mock.getSentMail();
@@ -175,7 +175,7 @@ test('Confirmed by admin email', async () => {
 
 describe('sendEmail function', () => {
     test('Send email, arbitrary email', async () => {
-        await sendEmail({ 
+        await sendEmail({
             mailText: 'Test Text',
             mailSubject: 'Test subject',
             toUser: {
@@ -185,7 +185,7 @@ describe('sendEmail function', () => {
                 displayName: defaultUserData.email
             }
         }, {});
-    
+
         const sentEmails = nodemailerMock.mock.getSentMail();
         expect(sentEmails.length).toBe(1);
         expect(sentEmails[0].from).toEqual(fromEmail);
@@ -194,9 +194,9 @@ describe('sendEmail function', () => {
         expect(sentEmails[0].text).toEqual('Test Text');
         expect(sentEmails[0].html).toEqual('Test Text');
     });
-    
+
     test('Send email, arbitrary email with translation array', async () => {
-        await sendEmail({ 
+        await sendEmail({
             mailText: ['translationNs1:textString', 'translationNs2:textString'],
             mailSubject: ['translationNs1:subjectString', 'translationNs2:subjectString'],
             toUser: {
@@ -206,7 +206,7 @@ describe('sendEmail function', () => {
                 displayName: defaultUserData.email
             }
         }, {});
-    
+
         const sentEmails = nodemailerMock.mock.getSentMail();
         expect(sentEmails.length).toBe(1);
         expect(sentEmails[0].from).toEqual(fromEmail);
