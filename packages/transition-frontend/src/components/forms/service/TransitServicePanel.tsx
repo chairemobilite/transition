081901e93607a7107846679b120e6d6627df7a2f
Niang-04 <moustaphang04@gmail.com>
2025-01-29 17:49:54 -0500

    Add Lines Data

--- packages/transition-frontend/src/components/forms/service/TransitServicePanel.tsx
+++ packages/transition-frontend/src/components/forms/service/TransitServicePanel.tsx
@@ -17,11 +17,13 @@ import CollectionSaveToCacheButtons from '../../parts/CollectionSaveToCacheButto
 import ServiceEdit from './TransitServiceEdit';
 import ServicesList from './TransitServiceList';
 import ServicesImportForm from './TransitServicesImportForm';
+import LineCollection from 'transition-common/lib/services/line/LineCollection';
 
 // Using a state object instead of 2 useState hooks because we want this object
 // to be modified and cause a re-render if the selection or collection was
 // updated, even if the pointer to the collection/selected object do not change.
 interface ServicePanelState {
+    lineCollection: LineCollection;
     serviceCollection: ServiceCollection;
     selectedService?: Service;
 }
@@ -29,40 +31,58 @@ interface ServicePanelState {
 const ServicesPanel: React.FunctionComponent<WithTranslation> = (props: WithTranslation) => {
     const [importerSelected, setImporterSelected] = React.useState(false);
     const [state, setState] = React.useState<ServicePanelState>({
+        lineCollection: serviceLocator.collectionManager.get('lines'),
         serviceCollection: serviceLocator.collectionManager.get('services'),
         selectedService: serviceLocator.selectedObjectsManager.get('service')
     });
-
-    const onServiceCollectionUpdate = () =>
-        setState(({ selectedService }) => ({
-            selectedService,
-            serviceCollection: serviceLocator.collectionManager.get('services')
-        }));
-    const onSelectedServiceUpdate = () =>
-        setState(({ serviceCollection }) => ({
+    const onLineCollectionUpdate = () =>
+        setState(({ serviceCollection, selectedService }) => ({
             serviceCollection,
-            selectedService: serviceLocator.selectedObjectsManager.get('service')
+            selectedService,
+            lineCollection: serviceLocator.collectionManager.get('lines') 
+    }));
+    const onServiceCollectionUpdate = () =>
+        setState(({ selectedService, lineCollection }) => ({
+            selectedService,
+            serviceCollection: serviceLocator.collectionManager.get('services'),
+            lineCollection 
+    }));
+    const onSelectedServiceUpdate = () =>
+        setState(({ serviceCollection, lineCollection }) => ({
+            serviceCollection,
+            selectedService: serviceLocator.selectedObjectsManager.get('service'),
+            lineCollection 
         }));
 
     React.useEffect(() => {
         serviceLocator.eventManager.on('collection.update.services', onServiceCollectionUpdate);
         serviceLocator.eventManager.on('selected.update.service', onSelectedServiceUpdate);
         serviceLocator.eventManager.on('selected.deselect.service', onSelectedServiceUpdate);
+        serviceLocator.eventManager.on('collection.update.lines', onLineCollectionUpdate); 
         // Reload the service collections at mount time, to make sure it is up to date
         if (state.serviceCollection) {
             state.serviceCollection.loadFromServer(serviceLocator.socketEventManager, serviceLocator.collectionManager);
         }
+        if (state.lineCollection) {
+            state.lineCollection.loadFromServer(serviceLocator.socketEventManager, serviceLocator.collectionManager).then(() => {
+            });
+        } 
         return () => {
             serviceLocator.eventManager.off('collection.update.services', onServiceCollectionUpdate);
             serviceLocator.eventManager.off('selected.update.service', onSelectedServiceUpdate);
             serviceLocator.eventManager.off('selected.deselect.service', onSelectedServiceUpdate);
+            serviceLocator.eventManager.off('collection.update.lines', onLineCollectionUpdate); 
         };
     }, []);
 
     return (
         <div id="tr__form-transit-services-panel" className="tr__form-transit-services-panel tr__panel">
             {!state.selectedService && !importerSelected && (
-                <ServicesList selectedService={state.selectedService} serviceCollection={state.serviceCollection} />
+                <ServicesList
+                    selectedService={state.selectedService}
+                    serviceCollection={state.serviceCollection}
+                    lineCollection={state.lineCollection} 
+                />
             )}
 
             {state.selectedService && !importerSelected && (
