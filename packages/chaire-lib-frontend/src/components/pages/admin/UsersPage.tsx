a604eda8f3dba594a7c5d7df478804f2acff622f
Geneviève Bastien <gbastien@versatic.net>
2025-02-05 16:01:18 -0500

    users: fix `UsersPage` component with new React
    This page caused errors, with an invalid hook call to useNavigate in a
    constructor. We convert the page to a function component instead.
    
    And remove the `navigate` call that is not necessary

--- packages/chaire-lib-frontend/src/components/pages/admin/UsersPage.tsx
+++ packages/chaire-lib-frontend/src/components/pages/admin/UsersPage.tsx
@@ -4,17 +4,9 @@
  * This file is licensed under the MIT License.
  * License text available at https://opensource.org/licenses/MIT
  */
-import { CliUser } from 'chaire-lib-common/lib/services/user/userType';
 import React from 'react';
 import Loadable from 'react-loadable';
-import { useNavigate } from 'react-router';
 import Loader from 'react-spinners/HashLoader';
-
-export type UsersPageProps = {
-    isAuthenticated: boolean;
-    // TODO Type the user
-    user: CliUser;
-};
 
 const loader = function Loading() {
     return <Loader size={30} color={'#aaaaaa'} loading={true} />;
@@ -25,23 +17,10 @@ const UsersComponent = Loadable({
     loading: loader
 });
 
-class UsersPage extends React.Component<UsersPageProps> {
-    constructor(props: UsersPageProps) {
-        const navigate = useNavigate();
-        super(props);
-
-        if (this.props.isAuthenticated && this.props.user.is_admin) {
-            navigate('/admin/users');
-        }
-    }
-
-    render() {
-        return (
-            <div className="admin">
-                <UsersComponent />
-            </div>
-        );
-    }
-}
+const UsersPage: React.FC = () => (
+    <div className="admin">
+        <UsersComponent />
+    </div>
+);
 
 export default UsersPage;
