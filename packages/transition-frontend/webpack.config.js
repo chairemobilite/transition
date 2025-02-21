d523a0847d18e53a93d8ae0ee444234483e94a77
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2025-02-21 13:55:08 -0500

    update format for webpack.config using 4 spaces

--- packages/transition-frontend/webpack.config.js
+++ packages/transition-frontend/webpack.config.js
@@ -6,16 +6,16 @@
  */
 const fs = require('fs');
 const path = require('path');
-const webpack                 = require('webpack');
-const MiniCssExtractPlugin    = require("mini-css-extract-plugin");
-const CopyWebpackPlugin       = require('copy-webpack-plugin');
-const HtmlWebpackPlugin       = require('html-webpack-plugin');
-const { CleanWebpackPlugin }  = require("clean-webpack-plugin");
-const CompressionPlugin       = require('compression-webpack-plugin');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 require('chaire-lib-backend/lib/config/dotenv.config');
 
 if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = 'development';
+    process.env.NODE_ENV = 'development';
 }
 
 const configuration = require('chaire-lib-backend/lib/config/server.config');
@@ -29,169 +29,169 @@ const publicDirectory = path.join(__dirname, '..', '..', 'public');
 const bundleOutputPath = path.join(publicDirectory, 'dist', config.projectShortname);
 // Local path where locales are stored
 const localeDirectory = path.join(__dirname, '..', '..', 'locales');
-const entryFileName =  './lib/app-transition.js';
+const entryFileName = './lib/app-transition.js';
 
 module.exports = (env) => {
-  console.log(`building js for project ${config.projectShortname}`);
-  console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+    console.log(`building js for project ${config.projectShortname}`);
+    console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
-  const isProduction = process.env.NODE_ENV === 'production';
+    const isProduction = process.env.NODE_ENV === 'production';
 
-  // Make sure all builds have different names if not a production bundle
-  const bundleFileName = isProduction ? `transition-${config.projectShortname}-bundle-${process.env.NODE_ENV}.[contenthash].js` : `transition-${config.projectShortname}-bundle-${process.env.NODE_ENV}.dev.js`;
-  const styleFileName = isProduction ? `transition-${config.projectShortname}-styles.[contenthash].css` : `transition-${config.projectShortname}-styles.dev.css`;
-  // HTML main file name
-  const htmlFileName = path.join(`index-${config.projectShortname}.html`);
+    // Make sure all builds have different names if not a production bundle
+    const bundleFileName = isProduction ? `transition-${config.projectShortname}-bundle-${process.env.NODE_ENV}.[contenthash].js` : `transition-${config.projectShortname}-bundle-${process.env.NODE_ENV}.dev.js`;
+    const styleFileName = isProduction ? `transition-${config.projectShortname}-styles.[contenthash].css` : `transition-${config.projectShortname}-styles.dev.css`;
+    // HTML main file name
+    const htmlFileName = path.join(`index-${config.projectShortname}.html`);
 
-  const languages = config.languages || ['fr', 'en'];
-  const languagesFilter = `/${languages.join("|")}/`;
+    const languages = config.languages || ['fr', 'en'];
+    const languagesFilter = `/${languages.join('|')}/`;
 
-  // TODO Custom styles and locales should be set in config (#419, #420)
-  const customStylesFilePath  = `${config.projectDir}/styles/styles.scss`;
-  const customLocalesFilePath = `${config.projectDir}/locales`;
-  const entry                 = fs.existsSync('./'+customStylesFilePath) ? [entryFileName, './'+customStylesFilePath] : [entryFileName];
-  const includeDirectories    = [
-    path.join(__dirname, 'lib'),
+    // TODO Custom styles and locales should be set in config (#419, #420)
+    const customStylesFilePath = `${config.projectDir}/styles/styles.scss`;
+    const customLocalesFilePath = `${config.projectDir}/locales`;
+    const entry = fs.existsSync('./' + customStylesFilePath) ? [entryFileName, './' + customStylesFilePath] : [entryFileName];
+    const includeDirectories = [
+        path.join(__dirname, 'lib'),
 
-    path.join(__dirname, 'lib', entryFileName),
-    localeDirectory
-  ];
+        path.join(__dirname, 'lib', entryFileName),
+        localeDirectory
+    ];
 
-  return {
-    // Controls which information to display (see https://webpack.js.org/configuration/stats/)
-    stats: {
-      errorDetails: true,
-      children: true,
-    },
-    node: {
-      // global will be deprecated at next major release, see where it is being used
-      global: 'warn'
-    },
-    mode: process.env.NODE_ENV,
-    entry,
-    output: {
-      path: bundleOutputPath,
-      filename: bundleFileName,
-      publicPath: '/dist/'
-    },
-    watchOptions: {
-      ignored: ['node_modules/**'],
-      aggregateTimeout: 600
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
+    return {
+        // Controls which information to display (see https://webpack.js.org/configuration/stats/)
+        stats: {
+            errorDetails: true,
+            children: true,
         },
-        {
-          use: 'json-loader',
-          test: /\.geojson$/,
-          include: includeDirectories
+        node: {
+            // global will be deprecated at next major release, see where it is being used
+            global: 'warn'
         },
-        {
-          test: /\.(ttf|woff2|woff|eot|svg)$/,
-          type: 'asset'
+        mode: process.env.NODE_ENV,
+        entry,
+        output: {
+            path: bundleOutputPath,
+            filename: bundleFileName,
+            publicPath: '/dist/'
         },
-        {
-          test: /\.glsl$/,
-          use: 'ts-shader-loader'
+        watchOptions: {
+            ignored: ['node_modules/**'],
+            aggregateTimeout: 600
         },
-        {
-          test: /\.s?css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }
-            }
-          ]
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    use: 'json-loader',
+                    test: /\.geojson$/,
+                    include: includeDirectories
+                },
+                {
+                    test: /\.(ttf|woff2|woff|eot|svg)$/,
+                    type: 'asset'
+                },
+                {
+                    test: /\.glsl$/,
+                    use: 'ts-shader-loader'
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /locales/,
+                    loader: '@alienfast/i18next-loader',
+                    options: {
+                        basenameAsNamespace: true,
+                        overrides: (fs.existsSync('./' + customLocalesFilePath) ? ['../' + customLocalesFilePath] : [])
+                    }
+                }
+            ]
         },
-        {
-          test: /locales/,
-          loader: '@alienfast/i18next-loader',
-          options: {
-            basenameAsNamespace: true,
-            overrides: (fs.existsSync('./'+customLocalesFilePath) ? ['../'+customLocalesFilePath] : [])
-          }
+        plugins: [
+            new CleanWebpackPlugin({
+                dry: !isProduction,
+                verbose: true,
+                cleanAfterEveryBuildPatterns: ['**/*', '!images/**', '!*.html'],
+            }),
+            new HtmlWebpackPlugin({
+                filename: htmlFileName,
+                template: path.join(publicDirectory, 'index.html'),
+            }),
+            new MiniCssExtractPlugin({
+                filename: styleFileName
+            }),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'IS_BROWSER': JSON.stringify(true),
+                    'HOST': JSON.stringify(process.env.HOST),
+                    'TRROUTING_HOST': JSON.stringify(process.env.TRROUTING_HOST),
+                    'PROJECT_SOURCE': JSON.stringify(process.env.PROJECT_SOURCE),
+                    'IS_TESTING': JSON.stringify(process.env.NODE_ENV === 'test'),
+                    'GOOGLE_API_KEY': JSON.stringify(process.env.GOOGLE_API_KEY),
+                    'MAPBOX_ACCESS_TOKEN': JSON.stringify(process.env.MAPBOX_ACCESS_TOKEN),
+                    'MAPBOX_USER_ID': JSON.stringify(process.env.MAPBOX_USER_ID || config.mapboxUserId),
+                    'MAPBOX_STYLE_ID': JSON.stringify(process.env.MAPBOX_STYLE_ID || config.mapboxStyleId),
+                    'CUSTOM_RASTER_TILES_XYZ_URL': JSON.stringify(process.env.CUSTOM_RASTER_TILES_XYZ_URL || config.customRasterTilesXyzUrl),
+                    'CUSTOM_RASTER_TILES_MIN_ZOOM': JSON.stringify(process.env.CUSTOM_RASTER_TILES_MIN_ZOOM || config.customRasterTilesMinZoom),
+                    'CUSTOM_RASTER_TILES_MAX_ZOOM': JSON.stringify(process.env.CUSTOM_RASTER_TILES_MAX_ZOOM || config.customRasterTilesMaxZoom)
+                },
+                '__CONFIG__': JSON.stringify({
+                    ...config
+                })
+            }),
+            new webpack.optimize.AggressiveMergingPlugin(),//Merge chunks
+            new CompressionPlugin({
+                filename: '[path][base].gz[query]',
+                algorithm: 'gzip',
+                test: /\.js$|\.css$/,
+                threshold: 0,
+                minRatio: 0.8
+            }),
+            new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, new RegExp(languagesFilter)),
+            new webpack.ContextReplacementPlugin(/date\-fns[\/\\]/, new RegExp(languagesFilter)),
+            new CopyWebpackPlugin(
+                {
+                    patterns: [
+                        {
+                            context: path.join(__dirname, 'lib', 'assets'),
+                            from: '**/*',
+                            to: '',
+                            noErrorOnMissing: true
+                        }
+                    ]
+                }
+            )
+        ],
+        resolve: {
+            mainFields: ['browser', 'main', 'module'],
+            modules: ['node_modules'],
+            extensions: ['.json', '.js', '.ts', '.tsx'],
+            fallback: { path: false },
+        },
+        devtool: isProduction ? 'cheap-source-map' : 'eval-source-map',
+        devServer: {
+            contentBase: publicDirectory,
+            historyApiFallback: true,
+            publicPath: '/dist/' + config.projectShortname
         }
-      ]
-    },
-    plugins: [
-      new CleanWebpackPlugin({
-        dry: !isProduction,
-        verbose: true,
-        cleanAfterEveryBuildPatterns: ['**/*', '!images/**', '!*.html'],
-      }),
-      new HtmlWebpackPlugin({
-        filename: htmlFileName,
-        template: path.join(publicDirectory, 'index.html'),
-      }),
-      new MiniCssExtractPlugin({
-        filename: styleFileName
-      }),
-      new webpack.DefinePlugin({
-        'process.env': {
-          'IS_BROWSER'                  : JSON.stringify(true),
-          'HOST'                        : JSON.stringify(process.env.HOST),
-          'TRROUTING_HOST'              : JSON.stringify(process.env.TRROUTING_HOST),
-          'PROJECT_SOURCE'              : JSON.stringify(process.env.PROJECT_SOURCE),
-          'IS_TESTING'                  : JSON.stringify(process.env.NODE_ENV === 'test'),
-          'GOOGLE_API_KEY'              : JSON.stringify(process.env.GOOGLE_API_KEY),
-          'MAPBOX_ACCESS_TOKEN'         : JSON.stringify(process.env.MAPBOX_ACCESS_TOKEN),
-          'MAPBOX_USER_ID'              : JSON.stringify(process.env.MAPBOX_USER_ID || config.mapboxUserId),
-          'MAPBOX_STYLE_ID'             : JSON.stringify(process.env.MAPBOX_STYLE_ID || config.mapboxStyleId),
-          'CUSTOM_RASTER_TILES_XYZ_URL' : JSON.stringify(process.env.CUSTOM_RASTER_TILES_XYZ_URL || config.customRasterTilesXyzUrl),
-          'CUSTOM_RASTER_TILES_MIN_ZOOM': JSON.stringify(process.env.CUSTOM_RASTER_TILES_MIN_ZOOM || config.customRasterTilesMinZoom),
-          'CUSTOM_RASTER_TILES_MAX_ZOOM': JSON.stringify(process.env.CUSTOM_RASTER_TILES_MAX_ZOOM || config.customRasterTilesMaxZoom)
-        },
-        '__CONFIG__': JSON.stringify({
-            ...config
-        })
-      }),
-      new webpack.optimize.AggressiveMergingPlugin(),//Merge chunks
-      new CompressionPlugin({
-        filename: "[path][base].gz[query]",
-        algorithm: "gzip",
-        test: /\.js$|\.css$/,
-        threshold: 0,
-        minRatio: 0.8
-      }),
-      new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, new RegExp(languagesFilter)),
-      new webpack.ContextReplacementPlugin(/date\-fns[\/\\]/, new RegExp(languagesFilter)),
-      new CopyWebpackPlugin(
-        {
-          patterns: [
-            {
-              context: path.join(__dirname, 'lib', 'assets'),
-              from: "**/*",
-              to: "",
-              noErrorOnMissing: true
-            }
-          ]
-        }
-      )
-    ],
-    resolve: {
-      mainFields: ['browser', 'main', 'module'],
-      modules: ['node_modules'],
-      extensions: ['.json', '.js', '.ts', '.tsx'],
-      fallback: { path: false },
-    },
-    devtool: isProduction ? 'cheap-source-map' : 'eval-source-map',
-    devServer: {
-      contentBase: publicDirectory,
-      historyApiFallback: true,
-      publicPath: '/dist/' + config.projectShortname
-    }
-  };
+    };
 };
