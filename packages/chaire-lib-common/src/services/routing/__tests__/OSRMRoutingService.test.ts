ec016701c48e6cd88f80dacd5178f4690a58ac17
Geneviève Bastien <gbastien@versatic.net>
2025-02-27 11:34:35 -0500

    osrm: only return known and typed data in route results
    Part of #1263
    
    OSRM's API returns quite a lot of data with a route request, data that
    Transition does not make any use of currently and that cannot be
    generalized to our `RoutingResult` type. To avoid cluttering the result
    object with unnecessary data, the `OSRMRoutingService` maps only the
    known fields and drops other.
    
    FIXME If we need any additional fields, we can type them in the
    `Route` type in a engine independent way.
    
    To give ballpark numbers, simply by doing this, a 8km walking trip route
    result dropped from 48K to 18K.

--- packages/chaire-lib-common/src/services/routing/__tests__/OSRMRoutingService.test.ts
+++ packages/chaire-lib-common/src/services/routing/__tests__/OSRMRoutingService.test.ts
@@ -72,41 +72,77 @@ const stubPlaces = [{
     }
 }]
 
-
-test('Route', async () => {
-    const point1 = TestUtils.makePoint([-73.1, 45.1]);
-    const point2 =TestUtils.makePoint([-73.1, 44.9]);
-
-    const osrmWaypoint1 = {location: [-73.1, 45.1]}
-    const osrmWaypoint2 = {location: [-73.1, 44.9]}
-
-    // TODO fill the routes fields with something
-    const expectedResponse = {waypoints: [point1.geometry, point2.geometry], routes: []};
-    const osrmResponse = {waypoints: [osrmWaypoint1, osrmWaypoint2], routes: []};
-    const mockRoute = jest.fn().mockImplementation((params, callback) => callback(Status.createOk(osrmResponse)));
-    eventManager.on('service.osrmRouting.route', mockRoute);
+describe('Route', () => {
+    test('Route success', async () => {
+        const point1 = TestUtils.makePoint([-73.1, 45.1]);
+        const point2 =TestUtils.makePoint([-73.1, 44.9]);
     
-    const routeResult = await routingService.route({ mode: 'walking', points: { type: 'FeatureCollection', features: stubPlaces}, withAlternatives: true });
-    expect(mockRoute).toHaveBeenCalledTimes(1);
-    expect(mockRoute).toHaveBeenCalledWith({mode: 'walking', points: stubPlaces, annotations: true, steps: false, continue_straight: undefined, overview: "full", withAlternatives: true }, expect.any(Function))
-    expect(routeResult).toEqual(expectedResponse);
-});
+        const osrmWaypoint1 = {location: [-73.1, 45.1]}
+        const osrmWaypoint2 = {location: [-73.1, 44.9]}
+    
+        const osrmResponse = {waypoints: [osrmWaypoint1, osrmWaypoint2], routes: [{
+           distance: 5000,
+           duration: 3600,
+           geometry: { type: 'LineString', coordinates: [[-73.1, 45.1], [-73.1, 44.9]] },
+           weight: 50,
+           weight_name: 'testWeight',
+           legs: [{
+                distance: 5000,
+                duration: 3600,
+                weight: 50,
+                summary: 'something',
+                steps: [{
+                    distance: 500,
+                    duration: 360,
+                    geometry: { type: 'LineString', coordinates: [[-73.1, 45.1], [-73.1, 44.9]] }
+                }],
+                annotation: {
+                    distance: [100, 500],
+                    duration: [50, 250]
+                }
+           }]
+        }]};
+        const expectedResponse = {waypoints: [point1.geometry, point2.geometry], routes: [{
+            distance: osrmResponse.routes[0].distance,
+            duration: osrmResponse.routes[0].duration,
+            legs: [{
+                distance: osrmResponse.routes[0].legs[0].distance,
+                duration: osrmResponse.routes[0].legs[0].duration,
+                steps: [{
+                    distance: osrmResponse.routes[0].legs[0].steps[0].distance,
+                    duration: osrmResponse.routes[0].legs[0].steps[0].duration,
+                    geometry: osrmResponse.routes[0].legs[0].steps[0].geometry
+                }]
+            }],
+            geometry: osrmResponse.routes[0].geometry
+        }]};
+        const mockRoute = jest.fn().mockImplementation((params, callback) => callback(Status.createOk(osrmResponse)));
+        eventManager.on('service.osrmRouting.route', mockRoute);
+        
+        const routeResult = await routingService.route({ mode: 'walking', points: { type: 'FeatureCollection', features: stubPlaces}, withAlternatives: true });
+        expect(mockRoute).toHaveBeenCalledTimes(1);
+        expect(mockRoute).toHaveBeenCalledWith({mode: 'walking', points: stubPlaces, annotations: true, steps: false, continue_straight: undefined, overview: "full", withAlternatives: true }, expect.any(Function))
+        expect(routeResult).toEqual(expectedResponse);
+    });
+    
+    test('Route fail', async () => {
+    
+        const mockRoute = jest.fn().mockImplementation((params, callback) => callback(Status.createError("BAD RESULTS")));
+        eventManager.on('service.osrmRouting.route', mockRoute);
+    
+        // TODO Could use the Jest expect.toThrow(), but it's a bit complex to implement with async and parameters
+        let routeResult;
+        let haveThrown = false;
+        try {
+            routeResult = await routingService.route({mode: 'walking', points: { type: 'FeatureCollection', features: stubPlaces}});
+        } catch {
+            haveThrown = true;
+        }
+        expect(mockRoute).toHaveBeenCalledTimes(1);
+        expect(mockRoute).toHaveBeenCalledWith({ mode: 'walking', points: stubPlaces, annotations: true, steps: false, continue_straight: undefined, overview: "full", withAlternatives: false }, expect.any(Function))
+        expect(routeResult).toBeUndefined();
+        expect(haveThrown).toBe(true);
+    });
+})
 
-test('Route fail', async () => {
 
-    const mockRoute = jest.fn().mockImplementation((params, callback) => callback(Status.createError("BAD RESULTS")));
-    eventManager.on('service.osrmRouting.route', mockRoute);
-
-    // TODO Could use the Jest expect.toThrow(), but it's a bit complex to implement with async and parameters
-    let routeResult;
-    let haveThrown = false;
-    try {
-        routeResult = await routingService.route({mode: 'walking', points: { type: 'FeatureCollection', features: stubPlaces}});
-    } catch {
-        haveThrown = true;
-    }
-    expect(mockRoute).toHaveBeenCalledTimes(1);
-    expect(mockRoute).toHaveBeenCalledWith({ mode: 'walking', points: stubPlaces, annotations: true, steps: false, continue_straight: undefined, overview: "full", withAlternatives: false }, expect.any(Function))
-    expect(routeResult).toBeUndefined();
-    expect(haveThrown).toBe(true);
-});
