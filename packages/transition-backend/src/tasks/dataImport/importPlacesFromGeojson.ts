f4815e13888edd7c3d1420ed5340a5001c3e2421
Geneviève Bastien <gbastien@versatic.net>
2025-03-14 15:27:47 -0400

    osm: add task to import poi and residential data in database
    fixes #1289
    
    This tasks is the last task of the OSM import that was missing, to
    import the generated and cleaned data saved in a geojson file to the
    database.
    
    This adds a task, `4_importPlacesToDb.task.ts` in `transition-backend`
    that will automatically read the POIs and residential entrances file and
    create/update 2 data sources, identified by the data source direction,
    followed by ' Places' and ' Residences' respectively.
    
    This task uses an import function that takes a point geojson file and
    data source name and does the actual import, asking the user whether to
    overwrite any previous data source of the same name. This import
    function can later be re-used by other code paths to import places.

---
+++ packages/transition-backend/src/tasks/dataImport/importPlacesFromGeojson.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import GenericDataImportTask from 'chaire-lib-common/lib/tasks/dataImport/genericDataImportTask';
+import importGeojsonPlaces from '../../services/importers/PlacesDataSourceImporter';
+
+export default class ImportPlacesFromGeojson extends GenericDataImportTask {
+    constructor(fileManager: any) {
+        super(fileManager);
+    }
+
+    /**
+     * IN: A file {@link GenericDataImportTask#POINT_OF_INTEREST_FILE} and
+     * {@link GenericDataImportTask#RESIDENTIAL_ENTRANCES_FILE} containing the
+     * residences and poi points
+     *
+     * OUT: The tr_places table is populated with the data from the geojson files
+     *
+     * @param dataSourceDirectory The directory containing the data sources
+     */
+    protected async doRun(dataSourceDirectory: string): Promise<void> {
+        // Import from the point of interest file
+        const absoluteDsDir = this._importDir + dataSourceDirectory + '/';
+        const POIgeojsonFilePath = absoluteDsDir + GenericDataImportTask.POINT_OF_INTEREST_FILE;
+        const dataSourceName = `${dataSourceDirectory} Places`;
+
+        await importGeojsonPlaces(POIgeojsonFilePath, dataSourceName);
+
+        const residencesGeojsonFilePath = absoluteDsDir + GenericDataImportTask.RESIDENTIAL_ENTRANCES_FILE;
+        const residenceDataSourceName = `${dataSourceDirectory} Residences`;
+
+        await importGeojsonPlaces(residencesGeojsonFilePath, residenceDataSourceName);
+    }
+}
