c9f1a7569ea8f32b1fa5c00748f97feb5c64fbd3
Yannick Brosseau <yannick.brosseau@gmail.com>
2025-04-07 20:35:03 -0400

    Remove unused data type in Rust json2capnp
    We don't currently generate data for garage, household, person, station, unit and zone and don't have any medium term
    to bring them back so remove them so we don't have to maintain them.

--- services/json2capnp/src/routers/station_collection_router.rs
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2022 Polytechnique Montreal and contributors
- *
- * This software may be used and distributed according to the terms of the
- * GNU General Public License version 2 or any later version.
- *
- */
-
