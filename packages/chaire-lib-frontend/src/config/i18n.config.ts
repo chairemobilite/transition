19f46528e20e6cb291df60d0a597e2413a3ff31f
Geneviève Bastien <gbastien@versatic.net>
2025-05-06 09:05:34 -0400

    dep: Remove dependency to `moment-business-days`
    fixes #1129
    
    The package is 5 years old and not necessary in Transition

--- packages/chaire-lib-frontend/src/config/i18n.config.ts
+++ packages/chaire-lib-frontend/src/config/i18n.config.ts
@@ -6,7 +6,7 @@
  */
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import moment from 'moment-business-days';
+import moment from 'moment';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
 
