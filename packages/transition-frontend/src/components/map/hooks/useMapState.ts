57a57bbe6f20ad7a47057fca1376500e8ce9be83
Geneviève Bastien <gbastien@versatic.net>
2025-05-16 09:22:11 -0400

    Add back the reset view state button

--- packages/transition-frontend/src/components/map/hooks/useMapState.ts
+++ packages/transition-frontend/src/components/map/hooks/useMapState.ts
@@ -18,11 +18,11 @@ import {
 const THROTTLE_DELAY = 25; // milliseconds
 
 export const useMapState = (initialCenter: [number, number], initialZoom: number) => {
-    const [viewState, setViewState] = useState(getDefaultViewState(initialCenter, initialZoom));
+    const [initialViewState, setInitialViewState] = useState(getDefaultViewState(initialCenter, initialZoom));
     const zoomRef = useRef<number>(initialZoom);
     const viewportRef = useRef<WebMercatorViewport | null>(null);
     const stateHasBeenSet = useRef(false);
-    const viewStateRef = useRef(viewState);
+    const viewStateRef = useRef(initialViewState);
 
     // Update viewport reference with current view state
     const updateViewportRef = useCallback((newViewState) => {
@@ -31,8 +31,8 @@ export const useMapState = (initialCenter: [number, number], initialZoom: number
 
     // Update the ref whenever viewState changes
     useEffect(() => {
-        viewStateRef.current = viewState;
-    }, [viewState]);
+        viewStateRef.current = initialViewState;
+    }, [initialViewState]);
 
     // Throttled preference updates once per second
     const throttledUpdatePref = useCallback(
@@ -64,7 +64,7 @@ export const useMapState = (initialCenter: [number, number], initialZoom: number
     const throttledSetViewState = useCallback(
         _throttle((newViewState) => {
             updateViewportRef(newViewState);
-            setViewState(newViewState);
+            setInitialViewState(newViewState);
             //updateUserPrefs(newViewState);
             zoomRef.current = newViewState.zoom;
         }, THROTTLE_DELAY),
@@ -79,6 +79,7 @@ export const useMapState = (initialCenter: [number, number], initialZoom: number
                 throttledSetViewState(newViewState);
                 stateHasBeenSet.current = true;
             } else if (viewStateHasChanged(oldViewState, newViewState)) {
+                // FIXME Ideally we wouldn't want to manage the zoom part of the state, but we need to make sure layers are updated when zooming. Find a better way to tell the layers to redraw.
                 throttledSetViewState(newViewState);
             }
             if (shouldUpdatePreferences(oldViewState, newViewState)) {
@@ -105,7 +106,7 @@ export const useMapState = (initialCenter: [number, number], initialZoom: number
 
     // Fit bounds handler
     const fitBounds = useCallback((bounds: [[number, number], [number, number]]) => {
-        setViewState((prevViewState) => {
+        setInitialViewState((prevViewState) => {
             const viewport = new WebMercatorViewport(prevViewState).fitBounds(bounds, {
                 padding: 20
             });
@@ -123,19 +124,37 @@ export const useMapState = (initialCenter: [number, number], initialZoom: number
         });
     }, []);
 
+    const flyTo = useCallback(({ latitude, longitude, zoom }) => {
+        setInitialViewState((prevViewState) => {
+            const newViewState = {
+                ...prevViewState,
+                latitude,
+                longitude,
+                zoom
+                // FIXME It would be nice to have a transition with transitionInterpolator: new FlyToInterpolator({speed: 2}), but our state managements clashes with the transition because the state is changed during the transition and any mouse move in the meantime will cause the transition to stop. See if we can prevent our state update during transition, if there's an event marking its end.
+            };
+            const viewport = new WebMercatorViewport(newViewState);
+            zoomRef.current = newViewState.zoom;
+            viewportRef.current = viewport;
+            throttledUpdatePref(newViewState);
+            return newViewState;
+        });
+    }, []);
+
     useEffect(() => {
         // Also initialize viewport if needed
         if (!viewportRef.current) {
-            viewportRef.current = new WebMercatorViewport(viewState);
+            viewportRef.current = new WebMercatorViewport(initialViewState);
         }
     }, []); // Empty dependency array means this runs once after initial render
 
     return {
-        viewState,
+        viewState: initialViewState,
         zoomRef,
         viewportRef,
         onViewStateChange,
         onResize,
-        fitBounds
+        fitBounds,
+        flyTo
     };
 };
