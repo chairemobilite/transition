38af26bef18d4cbb7d2f4dabc99615d975f2ff86
Geneviève Bastien <gbastien@versatic.net>
2025-11-24 14:10:12 -0500

    networkDesign: add types for the evolutionary algorithm job
    Type the parameters for a transit network job in general.
    
    Add the specific types for the transit network design using the
    evolutionary algorithm.
    
    Add functions to create and replay such a job. Also add an empty
    executor for the job and a wrapper for it on the workerpool.

---
+++ packages/transition-backend/src/services/networkDesign/transitNetworkDesign/evolutionary/types.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2025, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import { ErrorMessage } from 'chaire-lib-common/lib/utils/TrError';
+import { TransitNetworkDesignParameters } from 'transition-common/lib/services/networkDesign/transit/TransitNetworkDesignParameters';
+import { AlgorithmConfigurationByType } from 'transition-common/lib/services/networkDesign/transit/algorithm';
+import { SimulationMethodConfiguration } from 'transition-common/lib/services/networkDesign/transit/simulationMethod';
+import { ExecutableJob } from '../../../executableJob/ExecutableJob';
+
+export type EvolutionaryTransitNetworkDesignJobParameters = {
+    transitNetworkDesignParameters: TransitNetworkDesignParameters;
+    algorithmConfiguration: AlgorithmConfigurationByType<'evolutionaryAlgorithm'>;
+    // FIXME Should we support more than one simulation method here, with weight for each as previously?
+    simulationMethod: SimulationMethodConfiguration;
+};
+
+export type EvolutionaryTransitNetworkDesignJobType = {
+    name: 'evolutionaryTransitNetworkDesign';
+    data: {
+        parameters: EvolutionaryTransitNetworkDesignJobParameters;
+        // TODO Type the results when actual implementation is done
+        results?: never;
+    };
+    files: { transitDemand: true; nodeWeight: true };
+};
+
+export type EvolutionaryTransitNetworkDesignJob = ExecutableJob<EvolutionaryTransitNetworkDesignJobType>;
+
+export interface EvolutionaryTransitNetworkDesignJobResult {
+    /** Status of the job: 'success' means the job has completedly successfully,
+     * 'failed' means there was an unrecoverable exception during execution,
+     * 'paused' means the job should exit, waiting for child jobs to execute and
+     * terminate */
+    status: 'success' | 'failed' | 'paused';
+    warnings: ErrorMessage[];
+    errors: ErrorMessage[];
+}
