894318d7f746c96794a7095062c557485bacfb95
Geneviève Bastien <gbastien@versatic.net>
2025-11-24 14:24:19 -0500

    networkDesign: update types for the `OdTripSimulationMethod` type
    Make the demand in line with the one from batchRoute calculations.
    Separate the simulation in 3 fields: demand, routing parameters for
    calculation and additional evaluation options to calculate the cost of a
    scenario. Add descriptors for each of those methods and update tests
    accordingly.
    
    In the backend, update the simulation method with the new parameters. It
    does not work anymore. Pseudo-code has been written to describe how the
    simulation method will have to run.

--- packages/transition-common/src/services/networkDesign/transit/simulationMethod/OdTripSimulationMethod.ts
+++ packages/transition-common/src/services/networkDesign/transit/simulationMethod/OdTripSimulationMethod.ts
@@ -5,43 +5,113 @@
  * License text available at https://opensource.org/licenses/MIT
  */
 
+import { TransitRoutingBaseAttributes } from 'chaire-lib-common/lib/services/routing/types';
+import Preferences from 'chaire-lib-common/lib/config/Preferences';
 import { SimulationAlgorithmDescriptor } from '../TransitNetworkDesignAlgorithm';
+import { CsvFieldMappingDescriptor, CsvFileAndFieldMapper, CsvFileAndMapping } from '../../../csv';
+import { demandFieldDescriptors } from '../../../transitDemand/TransitOdDemandFromCsv';
+import { ErrorMessage } from 'chaire-lib-common/lib/utils/TrError';
 
-// Define OD trip simulation options
-export type OdTripSimulationOptions = {
-    dataSourceId: string;
+type OdTripEvaluationOptions = {
+    // The percentage of the OD trip in the demand to use for the simulation
     sampleRatio: number;
+    /**
+     * Name fo the fitness function to use for a single trip
+     *
+     * FIXME These functions have hard coded number, we may need to allow to
+     * parameterize the various functions. Now, the functions are defined in the
+     * defaultPreferences, identified by name. See if we need a { type: string;
+     * parameters: ... } type
+     */
     odTripFitnessFunction: string;
+    /**
+     * Name of the fitness function to use to evaluate the simulation run
+     *
+     * FIXME These functions have hard coded number, we may need to allow to
+     * parameterize the various functions. Now, the functions are defined in the
+     * defaultPreferences, identified by name. See if we need a { type: string;
+     * parameters: ... } type
+     */
     fitnessFunction: string;
 };
 
-/**
- * Descriptor class for the OD trip simulation method options. It documents the
- * options, types, validation and default values. It also validates the whole
- * options object.
- *
- * The OD Trip simulation method simulates the network using origin-destination
- * trip routing results.
- */
-export class OdTripSimulationDescriptor implements SimulationAlgorithmDescriptor<OdTripSimulationOptions> {
-    getTranslatableName = (): string => 'transit:simulation:simulationMethods:OdTrips';
+// Define OD trip simulation options
+export type OdTripSimulationOptions = {
+    // Describe where to get the OD trip data for the simulation
+    demandAttributes: CsvFileAndMapping;
+    // Transit routing parameters to use for the simulation
+    transitRoutingAttributes: TransitRoutingBaseAttributes;
+    evaluationOptions: OdTripEvaluationOptions;
+};
 
-    // TODO Add help texts
-    getOptions = () => ({
-        dataSourceId: {
-            i18nName: 'transit:simulation:simulationMethods:OdTripsDataSource',
-            type: 'select' as const,
-            choices: async () => {
-                // FIXME Still using data source queries. When this code was in the
-                // backend, it used the query to fetch the data source, now let's just
-                // use an empty array (this won't work, but it already doesn't work)
-                const dataSources: { id: string; shortname: string }[] = [];
-                return dataSources.map((ds) => ({
-                    value: ds.id,
-                    label: ds.shortname
-                }));
+class TransitRoutingAttributesDescriptor implements SimulationAlgorithmDescriptor<TransitRoutingBaseAttributes> {
+    getTranslatableName = (): string => 'transit:simulation:simulationMethods:transitRoutingAttributes';
+
+    getOptions = () => {
+        const transitRoutingAttributesDefaultsFromPref = Preferences.get(
+            'transit.routing.transit'
+        ) as TransitRoutingBaseAttributes;
+        return {
+            minWaitingTimeSeconds: {
+                i18nName: 'transit:simulation:minWaitingTimeSeconds',
+                type: 'integer' as const,
+                validate: (value: number) => value >= 0,
+                default: transitRoutingAttributesDefaultsFromPref.minWaitingTimeSeconds
+            },
+            maxTransferTravelTimeSeconds: {
+                i18nName: 'transit:simulation:maxTransferTravelTimeSeconds',
+                type: 'integer' as const,
+                validate: (value: number) => value >= 0,
+                default: transitRoutingAttributesDefaultsFromPref.maxTransferTravelTimeSeconds
+            },
+            maxAccessEgressTravelTimeSeconds: {
+                i18nName: 'transit:simulation:maxAccessEgressTravelTimeSeconds',
+                type: 'integer' as const,
+                validate: (value: number) => value >= 0,
+                default: transitRoutingAttributesDefaultsFromPref.maxAccessEgressTravelTimeSeconds
+            },
+            maxWalkingOnlyTravelTimeSeconds: {
+                i18nName: 'transit:simulation:maxWalkingOnlyTravelTimeSeconds',
+                type: 'integer' as const,
+                validate: (value: number) => value >= 0,
+                default: transitRoutingAttributesDefaultsFromPref.maxWalkingOnlyTravelTimeSeconds
+            },
+            maxFirstWaitingTimeSeconds: {
+                i18nName: 'transit:simulation:maxFirstWaitingTimeSeconds',
+                type: 'integer' as const,
+                validate: (value: number) => value >= 0,
+                default: transitRoutingAttributesDefaultsFromPref.maxFirstWaitingTimeSeconds
+            },
+            maxTotalTravelTimeSeconds: {
+                i18nName: 'transit:simulation:maxTotalTravelTimeSeconds',
+                type: 'integer' as const,
+                validate: (value: number) => value >= 0,
+                default: transitRoutingAttributesDefaultsFromPref.maxTotalTravelTimeSeconds
+            },
+            walkingSpeedMps: {
+                i18nName: 'transit:simulation:walkingSpeedMps',
+                type: 'number' as const,
+                validate: (value: number) => value > 0,
+                default: transitRoutingAttributesDefaultsFromPref.walkingSpeedMps
+            },
+            walkingSpeedFactor: {
+                i18nName: 'transit:simulation:walkingSpeedFactor',
+                type: 'number' as const,
+                validate: (value: number) => value > 0,
+                default: transitRoutingAttributesDefaultsFromPref.walkingSpeedFactor
             }
-        },
+        };
+    };
+
+    validateOptions = (_options: Partial<TransitRoutingBaseAttributes>): { valid: boolean; errors: string[] } => {
+        return { valid: true, errors: [] };
+    };
+}
+
+class SimulationOptionsDescriptor implements SimulationAlgorithmDescriptor<OdTripEvaluationOptions> {
+    getTranslatableName = (): string => 'transit:simulation:simulationMethods:simulationOptions';
+
+    getOptions = () => ({
         sampleRatio: {
             i18nName: 'transit:simulation:simulationMethods:OdTripsSampleRatio',
             type: 'number' as const,
@@ -82,11 +152,75 @@ export class OdTripSimulationDescriptor implements SimulationAlgorithmDescriptor
         }
     });
 
-    validateOptions = (_options: Partial<OdTripSimulationOptions>): { valid: boolean; errors: string[] } => {
-        const valid = true;
-        const errors: string[] = [];
+    validateOptions = (_options: Partial<OdTripEvaluationOptions>): { valid: boolean; errors: string[] } => {
+        return { valid: true, errors: [] };
+    };
+}
+
+// Add expansion factor to the demand field descriptors, and time is not required
+const demandFieldsWithoutTime = demandFieldDescriptors.filter((descriptor) => descriptor.key !== 'time');
+const timeFieldDescriptor: CsvFieldMappingDescriptor = demandFieldDescriptors.find((descriptor) => descriptor.key === 'time')!;
+const odDemandFieldDescriptors: CsvFieldMappingDescriptor[] = [
+    ...demandFieldsWithoutTime,
+    {
+        ...timeFieldDescriptor,
+        required: false
+    },
+    {
+        key: 'expansionFactor',
+        i18nLabel: 'transit:networkDesign:expansionFactorAttribute',
+        type: 'single',
+        required: false
+    }
+];
+/**
+ * Descriptor class for the OD trip simulation method options. It documents the
+ * options, types, validation and default values. It also validates the whole
+ * options object.
+ *
+ * The OD Trip simulation method simulates the network using origin-destination
+ * trip routing results.
+ */
+export class OdTripSimulationDescriptor implements SimulationAlgorithmDescriptor<OdTripSimulationOptions> {
+    getTranslatableName = (): string => 'transit:simulation:simulationMethods:OdTrips';
+
+    // TODO Add help texts
+    getOptions = () => ({
+        demandAttributes: {
+            i18nName: 'transit:simulation:simulationMethods:demandAttributes',
+            type: 'csvFile' as const,
+            mappingDescriptors: odDemandFieldDescriptors,
+            importFileName: 'transit_od_trips.csv'
+        },
+        transitRoutingAttributes: {
+            i18nName: 'transit:simulation:simulationMethods:transitRoutingAttributes',
+            type: 'nested' as const,
+            descriptor: () => new TransitRoutingAttributesDescriptor()
+        },
+        evaluationOptions: {
+            i18nName: 'transit:simulation:simulationMethods:simulationOptions',
+            type: 'nested' as const,
+            descriptor: () => new SimulationOptionsDescriptor()
+        }
+    });
+
+    validateOptions = (options: Partial<OdTripSimulationOptions>): { valid: boolean; errors: ErrorMessage[] } => {
+        let valid = true;
+        const errors: ErrorMessage[] = [];
 
         // TODO Actually validate something
+        // Validate the demand attributes
+        if (options.demandAttributes !== undefined) {
+            const demandFieldMappers = new CsvFileAndFieldMapper(
+                odDemandFieldDescriptors,
+                options.demandAttributes as CsvFileAndMapping
+            );
+            if (!demandFieldMappers.isValid()) {
+                valid = false;
+                errors.push(...demandFieldMappers.getErrors());
+            }
+        }
+        
 
         return { valid, errors };
     };
