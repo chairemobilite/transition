8226ddf89c650bc41ea159100604fddd7f5d6e86
Geneviève Bastien <gbastien@versatic.net>
2025-11-24 14:10:12 -0500

    networkDesign: add types for the evolutionary algorithm job
    Type the parameters for a transit network job in general.
    
    Add the specific types for the transit network design using the
    evolutionary algorithm.
    
    Add functions to create and replay such a job. Also add an empty
    executor for the job and a wrapper for it on the workerpool.

---
+++ packages/transition-backend/src/services/networkDesign/transitNetworkDesign/evolutionary/EvolutionaryTransitNetworkDesignJob.ts
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2025, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import { EventEmitter } from 'events';
+
+import type { EvolutionaryTransitNetworkDesignJob, EvolutionaryTransitNetworkDesignJobResult } from './types';
+import { evolutionaryAlgorithmFactory } from '../../../evolutionaryAlgorithm';
+
+/**
+ * Do batch calculation on a csv file input
+ *
+ * @param demandParameters The parameters for the batch calculation task
+ * @param batchRoutingQueryAttributes The transit routing parameters, for
+ * individual calculation
+ * @param options Options for this calculation: the absoluteBaseDirectory is the
+ * directory where the source files are and where the output files should be
+ * saved. The progress emitters allows to emit progress data to clients. The
+ * isCancelled function is periodically called to see if the task is cancelled.
+ * The currentCheckpoint, if specified, is the last checkpoint that was
+ * registered for this task. In batch routing, it represents the number of
+ * completed od trips routed.
+ * @returns
+ */
+
+export const runEvolutionaryTransitNetworkDesignJob = async (
+    job: EvolutionaryTransitNetworkDesignJob,
+    options: {
+        progressEmitter: EventEmitter;
+        isCancelled: () => boolean;
+    }
+): Promise<EvolutionaryTransitNetworkDesignJobResult> => {
+    return new EvolutionaryTransitNetworkDesignJobExecutor(job, options).run();
+};
+
+class EvolutionaryTransitNetworkDesignJobExecutor {
+    constructor(
+        private job: EvolutionaryTransitNetworkDesignJob,
+        private options: {
+            progressEmitter: EventEmitter;
+            isCancelled: () => boolean;
+        }
+    ) {
+        // Nothing else to do
+    }
+
+    run = async (): Promise<EvolutionaryTransitNetworkDesignJobResult> => {
+        // TODO Actually implement!! See ../simulation/SimulationExecution.ts file, the runSimulation function
+        try {
+            // Prepare the data: copy the main cache
+
+            // const algorithm = evolutionaryAlgorithmFactory(this.job.attributes.data.parameters.algorithmConfiguration.config, this.job);
+            return {
+                status: 'success',
+                warnings: [],
+                errors: []
+            };
+        } catch (error) {
+            console.log('error running evolutionary transit network design job', error);
+            return {
+                status: 'failed',
+                warnings: [],
+                errors: []
+            };
+        } finally {
+            // Do not cleanup all cache for now, as we may come back to this job
+            // later, after children have completed
+        }
+    };
+}
