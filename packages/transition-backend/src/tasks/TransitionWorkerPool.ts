18a6a75d25c78adc1ee6063e6b765fe5460072f3
Geneviève Bastien <gbastien@versatic.net>
2025-11-25 13:20:33 -0500

    networkDesign backend: replace SimulationRun by a job wrapper object
    All classes and functions are updated to take a transit network design
    job wrapper instance instead of a a SimulationRun with runtime data. It
    still does not work properly, but the types are correctly passed.

--- packages/transition-backend/src/tasks/TransitionWorkerPool.ts
+++ packages/transition-backend/src/tasks/TransitionWorkerPool.ts
@@ -18,8 +18,9 @@ import { BatchAccessMapJobType } from '../services/transitRouting/BatchAccessibi
 import { JobDataType } from 'transition-common/lib/services/jobs/Job';
 import Users from 'chaire-lib-backend/lib/services/users/users';
 import TrError from 'chaire-lib-common/lib/utils/TrError';
-import { EvolutionaryTransitNetworkDesignJob } from '../services/networkDesign/transitNetworkDesign/evolutionary/types';
+import { EvolutionaryTransitNetworkDesignJob, EvolutionaryTransitNetworkDesignJobType } from '../services/networkDesign/transitNetworkDesign/evolutionary/types';
 import { runEvolutionaryTransitNetworkDesignJob } from '../services/networkDesign/transitNetworkDesign/evolutionary/EvolutionaryTransitNetworkDesignJob';
+
 
 function newProgressEmitter(task: ExecutableJob<JobDataType>) {
     const eventEmitter = new EventEmitter();
