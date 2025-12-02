198a988e78e2041e183a55bb7b76e936cdc53a2c
Geneviève Bastien <gbastien@versatic.net>
2025-12-02 13:07:12 -0500

    networkDesign: let the transit network parameters use descriptor
    Instead of hard-coding the transit network parameters configuration
    form, it uses the OptionDescriptor to define the fields and their data
    and validations.

--- packages/transition-common/src/services/networkDesign/transit/TransitNetworkDesignParameters.ts
+++ packages/transition-common/src/services/networkDesign/transit/TransitNetworkDesignParameters.ts
@@ -4,6 +4,13 @@
  * This file is licensed under the MIT License.
  * License text available at https://opensource.org/licenses/MIT
  */
+import { ErrorMessage } from "chaire-lib-common/lib/utils/TrError";
+import { SimulationAlgorithmDescriptor } from "./TransitNetworkDesignAlgorithm";
+import serviceLocator from 'chaire-lib-common/lib/utils/ServiceLocator';
+import Agency from "../../agency/Agency";
+import Service from "../../service/Service";
+import Line from "../../line/Line";
+
 export type TransitNetworkDesignParameters = {
     /** Maximum number of minutes between passages */
     maxTimeBetweenPassages: number;
@@ -95,3 +102,95 @@ export const validateTransitNetworkDesignParameters = (
 
     return { valid, errors };
 };
+
+export class TransitNetworkDesignDescriptor implements SimulationAlgorithmDescriptor<TransitNetworkDesignParameters> {
+    
+    getTranslatableName(): string {
+        return 'transit:networkDesign:TransitNetworkDesignParameters';
+    }
+
+    getOptions() {
+        return {
+            numberOfLinesMin: {
+                i18nName: 'transit:networkDesign:parameters:NumberOfLinesMin',
+                type: 'integer' as const,
+                validate: (value: number) => value >= 1,
+                required: true
+            },
+            numberOfLinesMax: {
+                i18nName: 'transit:networkDesign:parameters:NumberOfLinesMax',
+                type: 'integer' as const,
+                validate: (value: number) => value >= 1,
+                required: true
+            },
+            maxTimeBetweenPassages: {
+                i18nName: 'transit:networkDesign:parameters:MaxIntervalMinutes',
+                type: 'integer' as const,
+                validate: (value: number) => value >= MIN_TIME_BETWEEN_PASSAGES,
+                default: 30,
+                required: true
+            },
+            minTimeBetweenPassages: {
+                i18nName: 'transit:networkDesign:parameters:MinIntervalMinutes',
+                type: 'integer' as const,
+                validate: (value: number) => value >= MIN_TIME_BETWEEN_PASSAGES,
+                default: 5,
+                required: true
+            },
+            nbOfVehicles: {
+                i18nName: 'transit:networkDesign:parameters:VehiclesCount',
+                type: 'integer' as const,
+                validate: (value: number) => value >= 1,
+                required: true
+            },
+            simulatedAgencies: {
+                i18nName: 'transit:networkDesign:parameters:LineSetAgencies',
+                type: 'multiselect' as const,
+                required: true,
+                choices: () => 
+                    serviceLocator.collectionManager.get('agencies')?.getFeatures().map((agency: Agency) => ({
+                        value: agency.getId(),
+                        label: agency.toString()
+                    })) || []
+            },
+            nonSimulatedServices: {
+                i18nName: 'transit:networkDesign:parameters:NonSimulatedServices',
+                type: 'multiselect' as const,
+                choices: () => 
+                    serviceLocator.collectionManager.get('services')?.getFeatures().map((service: Service) => ({
+                        value: service.getId(),
+                        label: service.attributes.name || service.getId()
+                    })) || []
+                
+            },
+            linesToKeep: {
+                i18nName: 'transit:networkDesign:parameters:KeepLines',
+                type: 'multiselect' as const,
+                choices: (object: Record<string, unknown>) => {
+                    const agencyCollection = serviceLocator.collectionManager.get('agencies')
+                    if (!agencyCollection) {
+                        return [];
+                    }
+                    const simulatedAgencies: string[] = object.simulatedAgencies as string[] || [];
+                    return simulatedAgencies.flatMap((agencyId: string) => {
+                        const agency: Agency | undefined = agencyCollection?.getById(agencyId);
+                        if (!agency) {
+                            return [];
+                        }
+                        const lines = agency.getLines();
+                        return lines.map((line: Line) => ({
+                            value: line.getId(),
+                            label: line.toString()
+                        }));
+                    });
+                }
+            }
+        };
+    }
+
+    validateOptions(options: Partial<TransitNetworkDesignParameters>): { valid: boolean; errors: ErrorMessage[] } {
+        return validateTransitNetworkDesignParameters(options);
+    }
+}
+
+export const transitNetworkDesignDescriptor = new TransitNetworkDesignDescriptor();
