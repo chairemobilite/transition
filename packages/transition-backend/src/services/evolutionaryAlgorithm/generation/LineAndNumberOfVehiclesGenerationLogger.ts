b7abf0fb0c03af9acd23e1abe7f02cef3fa8c5e1
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2025-12-15 11:23:55 -0500

    Fix linter errors

--- packages/transition-backend/src/services/evolutionaryAlgorithm/generation/LineAndNumberOfVehiclesGenerationLogger.ts
+++ packages/transition-backend/src/services/evolutionaryAlgorithm/generation/LineAndNumberOfVehiclesGenerationLogger.ts
@@ -6,13 +6,12 @@
  */
 import GenerationLogger from './GenerationLogger';
 import Generation from './Generation';
-import { fileManager } from 'chaire-lib-backend/lib/utils/filesystem/fileManager';
 
 class LineAndNumberOfVehiclesGenerationLogger extends GenerationLogger {
     /*
     Get a simulation generation and log results to a file
     */
-    doLog(generation: Generation): void {
+    doLog(_generation: Generation): void {
         // FIXME Implement or drop? Was commented when changed SimulationRun for Job
         /*const generationNumber = generation.getGenerationNumber();
         const bestCandidate = generation.getCandidates()[0];
