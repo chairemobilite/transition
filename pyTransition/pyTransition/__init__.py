7732c5ab8b6b36081bc8639a9027ed724d038c5e
Geneviève Bastien <gbastien@versatic.net>
2026-03-18 13:28:00 -0400

    pyTransition: bump version to 0.2.0
    Added parameters to the accessibility map API and a new type for
    departure or arrival time. But it is backward compatible

--- pyTransition/pyTransition/__init__.py
+++ pyTransition/pyTransition/__init__.py
@@ -6,6 +6,6 @@ A python package to interact with the Transition API.
 
 from .transition import Transition, TripTimeChoice
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Transition City"
 __credits__ = "Transition City"
