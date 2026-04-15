fe7ad107ac0d13dd9e03a44153e45e99afc422ad
Geneviève Bastien <gbastien@versatic.net>
2026-04-15 09:19:35 -0400

    tests: change 'Routing' section ui test to 'Stop nodes'
    Pass the expected panel title to the `clickLeftMenuTest` helper, as it
    is each caller's responsibility to know what to expect instead of having
    an exhaustive switch case in the helper, english only.
    
    The UI tests are now often failing in the CI. The `Routing` section
    displays a full loading panel until all data is loaded, so the title is
    not available yet. And despite a 30 seconds timeout, it still shows the
    panel as loading.
    
    The real fix would imply
    1- Refactor the form with complete loading page so the title and any
    other part not depending on the data can be shown while it is loading
    2- Let the UI tests wait for the data to be loaded before testing
    anything (and load less data at startup anyway)
    
    The current approach is acceptable for now, as the UI tests for
    Transition do not yet claim to do any actual testing, but just change to
    a few sections

--- packages/transition-frontend/ui-tests/test-left-menu.spec.ts
+++ packages/transition-frontend/ui-tests/test-left-menu.spec.ts
@@ -23,9 +23,16 @@ test.beforeAll(async ({ browser }) => {
 
 loginTestHelpers.startAndLoginAnonymously({ context, title: 'Transition' });
 
-// Click on a few sections of the left menu to check that the panel switching works properly.
-testHelpers.clickLeftMenuTest({ context, section: 'routing' });
-testHelpers.clickLeftMenuTest({ context, section: 'preferences' });
-testHelpers.clickLeftMenuTest({ context, section: 'agencies' });
+// Click on a few sections of the left menu to check that the panel switching
+// works properly.
+//
+// FIXME Some sections like Routing and Accessibility map have a full loading
+// panel until all data is loaded, so we cannot check the title.  Despite a 30
+// seconds timeout, the tests were still timing out with a loading panel in the
+// CI, so we are skipping the title check for those sections for now. Anyway, we
+// are not really testing anything here.
+testHelpers.clickLeftMenuTest({ context, section: 'nodes', expectedRightPanelTitle: 'Stop nodes' });
+testHelpers.clickLeftMenuTest({ context, section: 'preferences', expectedRightPanelTitle: 'Preferences' });
+testHelpers.clickLeftMenuTest({ context, section: 'agencies', expectedRightPanelTitle: 'Agencies' });
 
 testHelpers.logoutTest({ context });
