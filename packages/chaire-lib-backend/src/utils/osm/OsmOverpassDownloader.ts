17c6535b9d84b514db5fca6788a963308d1344d3
Io A. Daza-Dillon <io@dazadillon.io>
2026-09-12 21:12:39 -0400

    Validate that Overpass responses contain OSM elements
    Fixes #672
    
    There are several ways this issue could have been solved. This
    implementation puts the validation in fetchAndWriteXml. We could
    instead validate the response immediately after fetchAndWriteXml
    returns, but keeping the validation there avoids reading the file back
    from disk. Since OSM files can be quite large, this may make a
    difference.
    
    sax has been added for more accurate validation. If we wanted to
    avoid adding a dependency, we could instead check for strings such as
    '<node ' or '<way ' and assume they won't appear in comments. That would
    probably be good enough in practice.
    
    The sax solution is still not 100% accurate, since it does not validate
    the complete structure of the OSM file.
    
    Validation is not performed when downloading through downloadXml. JSON
    responses are not supported either. Both could be added easily, but they
    seem out of scope for now.

--- packages/chaire-lib-backend/src/utils/osm/OsmOverpassDownloader.ts
+++ packages/chaire-lib-backend/src/utils/osm/OsmOverpassDownloader.ts
@@ -14,6 +14,7 @@ import type { ReadableStream } from 'node:stream/web';
 import { geojsonToPolyBoundary } from 'chaire-lib-common/lib/utils/geometry/ConversionUtils';
 import allNodesXmlQuery from '../../config/osm/overpassQueries/allNodes';
 import allWaysAndRelationsXmlQuery from '../../config/osm/overpassQueries/allWaysAndRelations';
+import { createPassthroughTransform, ValidationTransformFactory } from './OsmValidationTransform';
 
 export interface OsmOverpassDownloader {
     downloadJson(boundPoly: GeoJSON.Polygon, overpassXmlQueryString: string): Promise<JSON>; // boundary polygon should not have holes!
@@ -239,15 +240,18 @@ class OsmOverpassDownloaderImpl implements OsmOverpassDownloader {
      * @param {*} [overpassXmlQueryString=allNodesXmlQuery] The XML query to
      * run. Placeholders BOUNDARY and OUTPUT will be replaced respectively by
      * the polygon boundaries in parameter and the output type, here 'xml'
+     * @param {ValidationTransformFactory} [createValidationTransform] Optional
+     * content validator.
      * @return {*} The result of the query, as an xml string
      * @memberof OsmOverpassDownloaderImpl
      */
     public async fetchAndWriteXml(
         filename: string,
         boundPoly: GeoJSON.Polygon | GeoJSON.FeatureCollection | GeoJSON.Feature,
-        overpassXmlQueryString = allNodesXmlQuery
+        overpassXmlQueryString = allNodesXmlQuery,
+        createValidationTransform?: ValidationTransformFactory
     ): Promise<boolean> {
-        return this.fetchAndWrite(filename, boundPoly, overpassXmlQueryString, 'xml');
+        return this.fetchAndWrite(filename, boundPoly, overpassXmlQueryString, 'xml', createValidationTransform);
     }
 
     /**
@@ -257,7 +261,8 @@ class OsmOverpassDownloaderImpl implements OsmOverpassDownloader {
         filename: string,
         boundPoly: GeoJSON.Polygon | GeoJSON.FeatureCollection | GeoJSON.Feature,
         overpassXmlQueryString: string,
-        fileType: 'xml' | 'json'
+        fileType: 'xml' | 'json',
+        createValidationTransform: ValidationTransformFactory = createPassthroughTransform
     ): Promise<boolean> {
         const response = await this.downloadData(boundPoly, overpassXmlQueryString, fileType);
         // Taken from fetch-node documentation
@@ -265,8 +270,16 @@ class OsmOverpassDownloaderImpl implements OsmOverpassDownloader {
         if (!response.body) {
             throw new Error('Response body is null');
         }
+        const validationTransform = createValidationTransform();
         // Type assertion needed to bridge Web Streams API and Node.js streams
-        await pipeline(Readable.fromWeb(response.body as ReadableStream<Uint8Array>), fs.createWriteStream(filename));
+        await pipeline(
+            Readable.fromWeb(response.body as ReadableStream<Uint8Array>),
+            validationTransform,
+            fs.createWriteStream(filename)
+        );
+        if (validationTransform.validationError) {
+            throw validationTransform.validationError;
+        }
         console.log('Done writing osm data');
         return true;
     }
